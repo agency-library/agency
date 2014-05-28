17eca7b4063064505a4389efe1507a5f9fa1c5a1
Jared Hoberock <jaredhoberock@gmail.com>
2014-05-28 15:18:01 -0700

    Add std::launch state to async_launcher.

--- launcher.hpp
+++ launcher.hpp
@@ -7,10 +7,14 @@
 class async_launcher
 {
   public:
+    async_launcher(std::launch policy)
+      : policy_(policy)
+    {}
+
     template<class Function, class... Args>
     std::future<void> async(Function&& f, Args&&... args) const
     {
-      return std::async(std::launch::async, std::forward<Function>(f), std::forward<Args>(args)...);
+      return std::async(policy_, std::forward<Function>(f), std::forward<Args>(args)...);
     }
 
     template<class Function, class... Args>
@@ -18,6 +22,9 @@ class async_launcher
     {
       std::forward<Function>(f)(std::forward<Args>(args)...);
     }
+
+  private:
+    std::launch policy_;
 };
 
 template<class Function, class... Args>
