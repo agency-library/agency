f7dc5cb4bbfce1d7e57033acfba71eaf99db45d4
Jared Hoberock <jaredhoberock@gmail.com>
2014-08-13 15:11:53 -0700

    Introduce vector_executor, vector_group, and vector_execution_policy. Add vector_saxpy.cpp example program

---
+++ vector_saxpy.cpp
@@ -0,0 +1,25 @@
+#include <execution_policy>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+int main()
+{
+  size_t n = 1 << 16;
+  std::vector<float> x(n, 1), y(n, 2), z(n);
+  float a = 13.;
+
+  std::bulk_invoke(std::vec(n), [&](std::vector_group<> &g)
+  {
+    int i = g.child().index();
+    z[i] = a * x[i] + y[i];
+  });
+
+  float expected  = a * 1. + 2.;
+  assert(std::all_of(z.begin(), z.end(), [=](float x){ return expected == x; }));
+
+  std::cout << "OK" << std::endl;
+
+  return 0;
+}
+
