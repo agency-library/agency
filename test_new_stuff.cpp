6b2cff411cbb8e18471620613cada083e0420125
Jared Hoberock <jaredhoberock@gmail.com>
2014-08-31 17:30:40 -0700

    Eliminate _new suffix on new versions of types. Fix #include problems in some headers

--- test_new_stuff.cpp
+++ test_new_stuff.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-  bulk_async(std::seq, [](std::sequential_agent_new& self)
+  bulk_async(std::seq, [](std::sequential_agent& self)
   {
     std::cout << "self.index(): " << self.index() << std::endl;
   }).wait();
 
   std::mutex mut;
-  bulk_invoke(std::con(10), [&mut](std::concurrent_agent_new& self)
+  bulk_invoke(std::con(10), [&mut](std::concurrent_agent& self)
   {
     mut.lock();
     std::cout << "self.index(): " << self.index() << " arriving at barrier" << std::endl;
@@ -22,17 +22,17 @@ int main()
     mut.unlock();
   });
 
-  bulk_async(std::seq(3, std::seq(2)), [](std::sequential_group_new<std::sequential_agent_new>& self)
+  bulk_async(std::seq(3, std::seq(2)), [](std::sequential_group<std::sequential_agent>& self)
   {
     std::cout << "index: (" << self.index() << ", " << self.inner().index() << ")" << std::endl;
   }).wait();
 
-  bulk_invoke(std::seq(4, std::seq(3, std::seq(2))), [](std::sequential_group_new<std::sequential_group_new<std::sequential_agent_new>>& self)
+  bulk_invoke(std::seq(4, std::seq(3, std::seq(2))), [](std::sequential_group<std::sequential_group<std::sequential_agent>>& self)
   {
     std::cout << "index: (" << self.index() << ", " << self.inner().index() << ", " << self.inner().inner().index() << ")" << std::endl;
   });
 
-  bulk_invoke(std::con(2, std::seq(2, std::con(2))), [&mut](std::concurrent_group_new<std::sequential_group_new<std::concurrent_agent_new>>& self)
+  bulk_invoke(std::con(2, std::seq(2, std::con(2))), [&mut](std::concurrent_group<std::sequential_group<std::concurrent_agent>>& self)
   {
     // the first agent in the first subgroup waits at the top-level barrier
     if(self.inner().index() == 0 && self.inner().inner().index() == 0)
