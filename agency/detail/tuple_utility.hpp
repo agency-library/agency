5c98bf346475f9aa10c607118604f8e518710d93
Jared Hoberock <jaredhoberock@gmail.com>
2014-11-20 14:34:08 -0800

    Update tuple_utility.hpp

--- agency/detail/tuple_utility.hpp
+++ agency/detail/tuple_utility.hpp
@@ -1,3 +1,29 @@
+// Copyright (c) 2014, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #pragma once
 
 #include <tuple>
@@ -387,7 +413,7 @@ typename std::enable_if<
 >::type
   tuple_for_each(Function f, Tuple1&& t1, Tuples&&... ts)
 {
-  ;
+  return;
 }
 
 
@@ -399,7 +425,7 @@ typename std::enable_if<
   tuple_for_each(Function f, Tuple1&& t1, Tuples&&... ts)
 {
   f(tuple_head(std::forward<Tuple1>(t1)), tuple_head(std::forward<Tuples>(ts))...);
-  tuple_for_each(f, forward_tuple_tail<Tuple1>(t1), forward_tuple_tail<Tuples>(ts)...);
+  return tuple_for_each(f, forward_tuple_tail<Tuple1>(t1), forward_tuple_tail<Tuples>(ts)...);
 }
 
 
