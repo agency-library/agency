ecee512ccd8499fb4e675f282faee88f962f2a26
Jared Hoberock <jaredhoberock@gmail.com>
2015-01-07 15:26:54 -0800

    Make index_cast recursive like shape_cast

--- agency/detail/index_cast.hpp
+++ agency/detail/index_cast.hpp
@@ -135,7 +135,7 @@ namespace index_cast_detail
 
 // to lift a point-like type, add a dimension
 template<class Index>
-struct lift_index_t_impl
+struct lift_t_impl
 {
   using type = rebind_point_size_t<Index, point_size<Index>::value + 1>;
 };
@@ -143,7 +143,7 @@ struct lift_index_t_impl
 
 // to lift a heterogeneous Tuple-like type, repeat the type of the last element
 template<template<class...> class tuple, class T, class... Types>
-struct lift_index_t_impl<tuple<T,Types...>>
+struct lift_t_impl<tuple<T,Types...>>
 {
   using last_type = typename std::tuple_element<sizeof...(Types)-1, std::tuple<Types...>>::type;
   using type = tuple<T,Types...,last_type>;
@@ -165,43 +165,134 @@ struct make
 } // end index_cast_detail
 
 
-template<class Index>
-using lift_index_t = typename index_cast_detail::lift_index_t_impl<Index>::type;
+template<class Point>
+using lift_t = typename index_cast_detail::lift_t_impl<Point>::type;
 
 
-template<class Index, class Shape>
+template<class Index, class FromShape, class ToShape>
 __AGENCY_ANNOTATION
-lift_index_t<Index> lift_index(const Index& idx, const Shape& shape)
+detail::tuple<lift_t<Index>, lift_t<FromShape>>
+  lift_index(const Index& idx, const FromShape& from_shape, const ToShape& to_shape)
 {
-  // to lift idx into shape,
-  // take the last element of idx and divide by the corresponding element of shape
+  // to lift idx into to_shape,
+  // take the last element of idx and divide by the corresponding element of to_shape
   // replace the last element of idx with the remainder and append the quotient
   const auto i = index_size<Index>::value - 1;
 
   auto idx_tuple = wrap_scalar(idx);
 
   auto intermediate_result = idx_tuple;
-  __tu::tuple_last(intermediate_result) %= detail::get<i>(shape);
+  __tu::tuple_last(intermediate_result) %= detail::get<i>(to_shape);
 
-  auto make_result = index_cast_detail::make<lift_index_t<Index>>{};
+  auto index_maker = index_cast_detail::make<lift_t<Index>>{};
 
-  return __tu::tuple_append_invoke(intermediate_result, __tu::tuple_last(idx_tuple) / detail::get<i>(shape), make_result);
+  auto lifted_index = __tu::tuple_append_invoke(intermediate_result, __tu::tuple_last(idx_tuple) / detail::get<i>(to_shape), index_maker);
+
+  // to lift from_shape, simply append the element of to_shape we just divided by
+  auto shape_maker = index_cast_detail::make<lift_t<FromShape>>{};
+  auto lifted_shape = __tu::tuple_append_invoke(wrap_scalar(from_shape), detail::get<i>(to_shape), shape_maker);
+
+  return detail::make_tuple(lifted_index, lifted_shape);
 }
 
 
+// index_cast is recursive and has various overloads
+// declare them here before their definitions
 
-// when both index types are the same size, index_cast is the identity operation
+// scalar -> scalar (base case)
 template<class ToIndex, class FromIndex, class FromShape, class ToShape>
 __AGENCY_ANNOTATION
 typename std::enable_if<
-  (index_size<FromIndex>::value == index_size<ToIndex>::value),
+  (index_size<FromIndex>::value == index_size<ToIndex>::value) &&
+  (index_size<FromIndex>::value == 1),
+  ToShape
+>::type
+  index_cast(const FromIndex& from_idx, const FromShape& from_shape, const ToShape& to_shape);
+
+
+// recursive case for casting two indices of equal size
+template<class ToIndex, class FromIndex, class FromShape, class ToShape>
+__AGENCY_ANNOTATION
+typename std::enable_if<
+  (index_size<FromIndex>::value == index_size<ToIndex>::value) &&
+  (index_size<FromIndex>::value > 1),
   ToIndex
 >::type
   index_cast(const FromIndex& from_idx,
-             const FromShape&,
-             const ToShape&)
+             const FromShape& from_shape,
+             const ToShape& to_shape);
+
+
+// upcast (recursive)
+template<class ToIndex, class FromIndex, class FromShape, class ToShape>
+__AGENCY_ANNOTATION
+typename std::enable_if<
+  (index_size<FromIndex>::value < index_size<ToIndex>::value),
+  ToIndex
+>::type
+  index_cast(const FromIndex& from_idx,
+             const FromShape& from_shape,
+             const ToShape&   to_shape);
+
+
+// downcast (recursive)
+template<class ToIndex, class FromIndex, class FromShape, class ToShape>
+typename std::enable_if<
+  (index_size<FromIndex>::value > index_size<ToIndex>::value),
+  ToIndex
+>::type
+  index_cast(const FromIndex& from_idx,
+             const FromShape& from_shape,
+             const ToShape& to_shape);
+
+
+// definitions of index_cast follow
+
+
+// terminal case for casting indices of size 1
+template<class ToIndex, class FromIndex, class FromShape, class ToShape>
+__AGENCY_ANNOTATION
+typename std::enable_if<
+  (index_size<FromIndex>::value == index_size<ToIndex>::value) &&
+  (index_size<FromIndex>::value == 1),
+  ToShape
+>::type
+  index_cast(const FromIndex& from_idx, const FromShape&, const ToShape&)
 {
-  return __tu::make_from_tuple<ToIndex>(wrap_scalar(from_idx));
+  // from_idx might not be a tuple, but instead a scalar type
+  // to ensure we can get the 0th value from from_idx in a uniform way, wrap it first
+  return static_cast<ToIndex>(detail::get<0>(wrap_scalar(from_idx)));
+}
+
+
+struct index_cast_functor
+{
+  template<class ToIndex, class FromIndex, class FromShape, class ToShape>
+  __AGENCY_ANNOTATION
+  auto operator()(const ToIndex&, const FromIndex& from_idx, const FromShape& from_shape, const ToShape& to_shape)
+    -> decltype(
+          index_cast<ToIndex>(from_idx, from_shape, to_shape)
+       )
+  {
+    return index_cast<ToIndex>(from_idx, from_shape, to_shape);
+  }
+};
+
+
+
+// when both index types are the same size, index_cast recursively maps itself across the tuples
+template<class ToIndex, class FromIndex, class FromShape, class ToShape>
+__AGENCY_ANNOTATION
+typename std::enable_if<
+  (index_size<FromIndex>::value == index_size<ToIndex>::value) &&
+  (index_size<FromIndex>::value > 1),
+  ToIndex
+>::type
+  index_cast(const FromIndex& from_idx,
+             const FromShape& from_shape,
+             const ToShape& to_shape)
+{
+  return __tu::tuple_map_with_make(index_cast_functor{}, index_cast_detail::make<ToIndex>{}, ToIndex{}, from_idx, from_shape, to_shape);
 }
 
 
@@ -216,7 +307,8 @@ typename std::enable_if<
              const FromShape& from_shape,
              const ToShape&   to_shape)
 {
-  return index_cast<ToIndex>(lift_index(from_idx, to_shape), from_shape, to_shape);
+  auto lifted_idx_and_shape = lift_index(from_idx, from_shape, to_shape);
+  return index_cast<ToIndex>(detail::get<0>(lifted_idx_and_shape), detail::get<1>(lifted_idx_and_shape), to_shape);
 }
 
 
