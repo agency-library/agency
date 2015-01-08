b521d311e586703c664dd868b1fa7766a9885ada
Jared Hoberock <jaredhoberock@gmail.com>
2015-01-07 16:35:50 -0800

    Get the multidimensional grid() case to compile Still takes ~30 minutes though

--- cuda_grid.cu
+++ cuda_grid.cu
@@ -13,6 +13,10 @@ struct kernel
   __device__
   void operator()(agency::parallel_group_2d<agency::cuda::concurrent_agent_2d>& self)
   {
+    auto outer_idx = self.outer().index();
+    auto inner_idx = self.inner().index();
+
+    printf("hello from {{%d, %d}, {%d, %d}}\n", (int)outer_idx[0], (int)outer_idx[1], (int)inner_idx[0], (int)inner_idx[1]);
   }
 };
 
@@ -26,32 +30,49 @@ auto grid(size_t num_blocks, size_t num_threads)
 }
 
 
-//using parallel_execution_policy_2d = agency::cuda::detail::basic_execution_policy<agency::cuda::parallel_agent_2d, agency::cuda::parallel_executor>;
-//const parallel_execution_policy_2d par_2d{};
-//
-//using concurrent_execution_policy_2d = agency::cuda::detail::basic_execution_policy<agency::cuda::concurrent_agent_2d, agency::cuda::block_executor>;
-//const concurrent_execution_policy_2d con_2d{};
-//
-//// XXX eliminate the need for these specializations
-//namespace agency
-//{
-//
-//template<>
-//struct is_execution_policy<parallel_execution_policy_2d> : std::true_type {};
-//
-//template<>
-//struct is_execution_policy<concurrent_execution_policy_2d> : std::true_type {};
-//
-//}
-//
-//
-//auto grid(agency::size2 num_blocks, agency::size2 num_threads)
-//  -> decltype(
-//       par_2d(num_blocks, con_2d(num_threads))
-//     )
-//{
-//  return par_2d(num_blocks, con_2d(num_threads));
-//}
+namespace agency
+{
+namespace cuda
+{
+
+
+class parallel_execution_policy_2d : public detail::basic_execution_policy<cuda::parallel_agent_2d, cuda::parallel_executor, parallel_execution_tag, parallel_execution_policy_2d>
+{
+  public:
+    using detail::basic_execution_policy<cuda::parallel_agent_2d, cuda::parallel_executor, parallel_execution_tag, parallel_execution_policy_2d>::basic_execution_policy;
+};
+
+const parallel_execution_policy_2d par_2d{};
+
+
+class concurrent_execution_policy_2d : public detail::basic_execution_policy<cuda::concurrent_agent_2d, cuda::block_executor, concurrent_execution_tag, concurrent_execution_policy_2d>
+{
+  public:
+    using detail::basic_execution_policy<cuda::concurrent_agent_2d, cuda::block_executor, concurrent_execution_tag, concurrent_execution_policy_2d>::basic_execution_policy;
+};
+
+const concurrent_execution_policy_2d con_2d{};
+
+
+} // end cuda
+
+
+template<>
+struct is_execution_policy<cuda::parallel_execution_policy_2d> : std::true_type {};
+
+template<>
+struct is_execution_policy<cuda::concurrent_execution_policy_2d> : std::true_type {};
+
+} // end agency
+
+
+auto grid(agency::size2 num_blocks, agency::size2 num_threads)
+  -> decltype(
+       agency::cuda::par_2d(num_blocks, agency::cuda::con_2d(num_threads))
+     )
+{
+  return agency::cuda::par_2d(num_blocks, agency::cuda::con_2d(num_threads));
+}
 
 
 int main()
