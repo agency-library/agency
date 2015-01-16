f40dc00233e6ac1e127a96b750b09e4bdc25841b
Jared Hoberock <jaredhoberock@gmail.com>
2015-01-15 16:18:41 -0800

    Simplify cuda_grid example

--- cuda_grid.cu
+++ cuda_grid.cu
@@ -30,48 +30,12 @@ auto grid(size_t num_blocks, size_t num_threads)
 }
 
 
-namespace agency
-{
-namespace cuda
-{
-
-
-class parallel_execution_policy_2d : public detail::basic_execution_policy<cuda::parallel_agent_2d, cuda::parallel_executor, parallel_execution_tag, parallel_execution_policy_2d>
-{
-  public:
-    using detail::basic_execution_policy<cuda::parallel_agent_2d, cuda::parallel_executor, parallel_execution_tag, parallel_execution_policy_2d>::basic_execution_policy;
-};
-
-const parallel_execution_policy_2d par_2d{};
-
-
-class concurrent_execution_policy_2d : public detail::basic_execution_policy<cuda::concurrent_agent_2d, cuda::block_executor, concurrent_execution_tag, concurrent_execution_policy_2d>
-{
-  public:
-    using detail::basic_execution_policy<cuda::concurrent_agent_2d, cuda::block_executor, concurrent_execution_tag, concurrent_execution_policy_2d>::basic_execution_policy;
-};
-
-const concurrent_execution_policy_2d con_2d{};
-
-
-} // end cuda
-
-
-template<>
-struct is_execution_policy<cuda::parallel_execution_policy_2d> : std::true_type {};
-
-template<>
-struct is_execution_policy<cuda::concurrent_execution_policy_2d> : std::true_type {};
-
-} // end agency
-
-
 auto grid(agency::size2 num_blocks, agency::size2 num_threads)
   -> decltype(
-       agency::cuda::par_2d(num_blocks, agency::cuda::con_2d(num_threads))
+       agency::cuda::par(num_blocks, agency::cuda::con(num_threads))
      )
 {
-  return agency::cuda::par_2d(num_blocks, agency::cuda::con_2d(num_threads));
+  return agency::cuda::par(num_blocks, agency::cuda::con(num_threads));
 }
 
 
