f19b6fc26d1a8e841c719d0f3f5849104076f2b8
Jared Hoberock <jaredhoberock@gmail.com>
2015-06-24 15:50:48 -0700

    Test all existing test executors in test_single_agent_execute.cpp

--- testing/test_single_agent_execute.cpp
+++ testing/test_single_agent_execute.cpp
@@ -2,39 +2,54 @@
 #include <cassert>
 #include <iostream>
 
-struct my_executor {};
+#include "test_executors.hpp"
 
-int main()
+template<class Executor>
+void test()
 {
+  using executor_type = Executor;
+
   {
     // returning void
-    my_executor exec;
+    executor_type exec;
 
     int set_me_to_thirteen = 0;
 
-    agency::new_executor_traits<my_executor>::execute(exec, [&]
+    agency::new_executor_traits<executor_type>::execute(exec, [&]
     {
       set_me_to_thirteen = 13;
     });
 
     assert(set_me_to_thirteen == 13);
+    assert(exec.valid());
   }
 
   {
     // returning int
-    my_executor exec;
+    executor_type exec;
 
-    auto result = agency::new_executor_traits<my_executor>::execute(exec, []
+    auto result = agency::new_executor_traits<executor_type>::execute(exec, []
     {
       return 13;
     });
 
     assert(result == 13);
+    assert(exec.valid());
   }
+}
+
+int main()
+{
+  using namespace test_executors;
+
+  test<empty_executor>();
+  test<single_agent_when_all_execute_and_select_executor>();
+  test<multi_agent_when_all_execute_and_select_executor>();
+  test<single_agent_then_execute_executor>();
+  test<multi_agent_execute_returning_user_defined_container_executor>();
 
   std::cout << "OK" << std::endl;
 
   return 0;
 }
-
 
