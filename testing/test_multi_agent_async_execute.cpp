b7a52ad54c0c5dc50d417e81a4a3d78797041166
Jared Hoberock <jaredhoberock@gmail.com>
2015-06-23 21:01:25 -0700

    Test all existing test executors in test_multi_agent_async_execute.cpp

--- testing/test_multi_agent_async_execute.cpp
+++ testing/test_multi_agent_async_execute.cpp
@@ -6,18 +6,21 @@
 #include <numeric>
 #include <mutex>
 
-struct my_executor {};
+#include "test_executors.hpp"
 
-int main()
+template<class Executor>
+void test()
 {
+  using executor_type = Executor;
+
   {
     // async_execute returning user-specified container
     
-    my_executor exec;
+    executor_type exec;
 
     size_t n = 100;
 
-    std::future<std::vector<int>> fut = agency::new_executor_traits<my_executor>::template async_execute<std::vector<int>>(exec, [](size_t idx)
+    std::future<std::vector<int>> fut = agency::new_executor_traits<executor_type>::template async_execute<std::vector<int>>(exec, [](size_t idx)
     {
       return idx;
     },
@@ -29,16 +32,17 @@ int main()
     std::iota(ref.begin(), ref.end(), 0);
 
     assert(std::equal(ref.begin(), ref.end(), result.begin()));
+    assert(exec.valid());
   }
 
   {
     // async_execute returning default container
     
-    my_executor exec;
+    executor_type exec;
 
     size_t n = 100;
 
-    auto fut = agency::new_executor_traits<my_executor>::async_execute(exec, [](size_t idx)
+    auto fut = agency::new_executor_traits<executor_type>::async_execute(exec, [](size_t idx)
     {
       return idx;
     },
@@ -50,18 +54,19 @@ int main()
     std::iota(ref.begin(), ref.end(), 0);
 
     assert(std::equal(ref.begin(), ref.end(), result.begin()));
+    assert(exec.valid());
   }
 
   {
     // async_execute returning void
     
-    my_executor exec;
+    executor_type exec;
 
     size_t n = 100;
 
     int increment_me = 0;
     std::mutex mut;
-    auto fut = agency::new_executor_traits<my_executor>::async_execute(exec, [&](size_t idx)
+    auto fut = agency::new_executor_traits<executor_type>::async_execute(exec, [&](size_t idx)
     {
       mut.lock();
       increment_me += 13;
@@ -72,7 +77,19 @@ int main()
     fut.wait();
 
     assert(increment_me == n * 13);
+    assert(exec.valid());
   }
+}
+
+int main()
+{
+  using namespace test_executors;
+
+  test<empty_executor>();
+  test<single_agent_when_all_execute_and_select_executor>();
+  test<multi_agent_when_all_execute_and_select_executor>();
+  test<single_agent_then_execute_executor>();
+  test<multi_agent_execute_returning_user_defined_container_executor>();
 
   std::cout << "OK" << std::endl;
 
