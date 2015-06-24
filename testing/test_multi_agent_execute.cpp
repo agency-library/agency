a321b6138669f79ee2791bdb877a3c3e3fbbcd0d
Jared Hoberock <jaredhoberock@gmail.com>
2015-06-23 21:07:01 -0700

    Test all existing test executors in test_multi_agent_execute.cpp

--- testing/test_multi_agent_execute.cpp
+++ testing/test_multi_agent_execute.cpp
@@ -6,18 +6,21 @@
 #include <numeric>
 #include <mutex>
 
-struct my_executor {};
+#include "test_executors.hpp"
 
-int main()
+template<class Executor>
+void test()
 {
+  using executor_type = Executor;
+
   {
     // execute returning user-specified container
     
-    my_executor exec;
+    executor_type exec;
 
     size_t n = 100;
 
-    std::vector<int> result = agency::new_executor_traits<my_executor>::template execute<std::vector<int>>(exec, [](size_t idx)
+    std::vector<int> result = agency::new_executor_traits<executor_type>::template execute<std::vector<int>>(exec, [](size_t idx)
     {
       return idx;
     },
@@ -27,16 +30,17 @@ int main()
     std::iota(ref.begin(), ref.end(), 0);
 
     assert(std::equal(ref.begin(), ref.end(), result.begin()));
+    assert(exec.valid());
   }
 
   {
     // execute returning default container
     
-    my_executor exec;
+    executor_type exec;
 
     size_t n = 100;
 
-    auto result = agency::new_executor_traits<my_executor>::execute(exec, [](size_t idx)
+    auto result = agency::new_executor_traits<executor_type>::execute(exec, [](size_t idx)
     {
       return idx;
     },
@@ -46,18 +50,19 @@ int main()
     std::iota(ref.begin(), ref.end(), 0);
 
     assert(std::equal(ref.begin(), ref.end(), result.begin()));
+    assert(exec.valid());
   }
 
   {
     // execute returning void
     
-    my_executor exec;
+    executor_type exec;
 
     size_t n = 100;
 
     int increment_me = 0;
     std::mutex mut;
-    agency::new_executor_traits<my_executor>::execute(exec, [&](size_t idx)
+    agency::new_executor_traits<executor_type>::execute(exec, [&](size_t idx)
     {
       mut.lock();
       increment_me += 13;
@@ -66,7 +71,19 @@ int main()
     n);
 
     assert(increment_me == n * 13);
+    assert(exec.valid());
   }
+}
+
+int main()
+{
+  using namespace test_executors;
+
+  test<empty_executor>();
+  test<single_agent_when_all_execute_and_select_executor>();
+  test<multi_agent_when_all_execute_and_select_executor>();
+  test<single_agent_then_execute_executor>();
+  test<multi_agent_execute_returning_user_defined_container_executor>();
 
   std::cout << "OK" << std::endl;
 
