549bc653586cb7ceeec3d3e1d5569236ef240690
Jared Hoberock <jaredhoberock@gmail.com>
2015-06-24 17:24:21 -0700

    Test multi_agent_execute_returning_void_executor with test_multi_agent_when_all_execute_and_select_with_shared_inits.cpp

--- testing/test_multi_agent_when_all_execute_and_select_with_shared_inits.cpp
+++ testing/test_multi_agent_when_all_execute_and_select_with_shared_inits.cpp
@@ -60,6 +60,7 @@ int main()
   test<when_all_executor>();
   test<multi_agent_execute_returning_user_defined_container_executor>();
   test<multi_agent_execute_returning_default_container_executor>();
+  test<multi_agent_execute_returning_void_executor>();
 
   std::cout << "OK" << std::endl;
 }
