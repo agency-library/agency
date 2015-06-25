4d4cc0272a371616d480d686d30681df5fe60f1f
Jared Hoberock <jaredhoberock@gmail.com>
2015-06-25 15:19:39 -0700

    Reenable disabled tests

--- testing/test_multi_agent_async_execute.cpp
+++ testing/test_multi_agent_async_execute.cpp
@@ -35,66 +35,66 @@ void test()
     assert(exec.valid());
   }
 
-//  {
-//    // async_execute returning default container
-//    
-//    executor_type exec;
-//
-//    size_t n = 100;
-//
-//    auto fut = agency::new_executor_traits<executor_type>::async_execute(exec, [](size_t idx)
-//    {
-//      return idx;
-//    },
-//    n);
-//
-//    auto result = fut.get();
-//
-//    std::vector<int> ref(n);
-//    std::iota(ref.begin(), ref.end(), 0);
-//
-//    assert(std::equal(ref.begin(), ref.end(), result.begin()));
-//    assert(exec.valid());
-//  }
-//
-//  {
-//    // async_execute returning void
-//    
-//    executor_type exec;
-//
-//    size_t n = 100;
-//
-//    int increment_me = 0;
-//    std::mutex mut;
-//    auto fut = agency::new_executor_traits<executor_type>::async_execute(exec, [&](size_t idx)
-//    {
-//      mut.lock();
-//      increment_me += 13;
-//      mut.unlock();
-//    },
-//    n);
-//
-//    fut.wait();
-//
-//    assert(increment_me == n * 13);
-//    assert(exec.valid());
-//  }
+  {
+    // async_execute returning default container
+    
+    executor_type exec;
+
+    size_t n = 100;
+
+    auto fut = agency::new_executor_traits<executor_type>::async_execute(exec, [](size_t idx)
+    {
+      return idx;
+    },
+    n);
+
+    auto result = fut.get();
+
+    std::vector<int> ref(n);
+    std::iota(ref.begin(), ref.end(), 0);
+
+    assert(std::equal(ref.begin(), ref.end(), result.begin()));
+    assert(exec.valid());
+  }
+
+  {
+    // async_execute returning void
+    
+    executor_type exec;
+
+    size_t n = 100;
+
+    int increment_me = 0;
+    std::mutex mut;
+    auto fut = agency::new_executor_traits<executor_type>::async_execute(exec, [&](size_t idx)
+    {
+      mut.lock();
+      increment_me += 13;
+      mut.unlock();
+    },
+    n);
+
+    fut.wait();
+
+    assert(increment_me == n * 13);
+    assert(exec.valid());
+  }
 }
 
 int main()
 {
   using namespace test_executors;
 
-//  test<empty_executor>();
-//  test<single_agent_when_all_execute_and_select_executor>();
-//  test<multi_agent_when_all_execute_and_select_executor>();
-//  test<single_agent_then_execute_executor>();
-//
-//  test<multi_agent_execute_returning_user_defined_container_executor>();
-//  test<multi_agent_execute_returning_default_container_executor>();
-//  test<multi_agent_execute_returning_void_executor>();
-//
-//  test<multi_agent_async_execute_returning_user_defined_container_executor>();
+  test<empty_executor>();
+  test<single_agent_when_all_execute_and_select_executor>();
+  test<multi_agent_when_all_execute_and_select_executor>();
+  test<single_agent_then_execute_executor>();
+
+  test<multi_agent_execute_returning_user_defined_container_executor>();
+  test<multi_agent_execute_returning_default_container_executor>();
+  test<multi_agent_execute_returning_void_executor>();
+
+  test<multi_agent_async_execute_returning_user_defined_container_executor>();
   test<multi_agent_async_execute_returning_default_container_executor>();
 
   std::cout << "OK" << std::endl;
