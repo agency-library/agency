ff972115e66bee1b99e61031864ae0c2bb9162fc
Jared Hoberock <jaredhoberock@gmail.com>
2015-07-07 20:21:58 -0700

    Implement multi_agent_then_execute_returning_user_specified_container with then_execute() with shared inits
    Test all executors in test_multi_agent_async_execute.cpp

--- agency/detail/executor_traits/multi_agent_then_execute_returning_user_specified_container.hpp
+++ agency/detail/executor_traits/multi_agent_then_execute_returning_user_specified_container.hpp
@@ -4,7 +4,7 @@
 #include <agency/future.hpp>
 #include <agency/new_executor_traits.hpp>
 #include <agency/detail/executor_traits/check_for_member_functions.hpp>
-#include <agency/detail/executor_traits/terminal_multi_agent_execute_returning_user_specified_container.hpp>
+#include <agency/detail/executor_traits/ignore_tail_parameters_and_invoke.hpp>
 #include <type_traits>
 #include <utility>
 
@@ -21,55 +21,14 @@ namespace multi_agent_then_execute_returning_user_specified_container_implementa
 
 struct use_multi_agent_then_execute_returning_user_specified_container_member_function {};
 
-struct use_multi_agent_when_all_execute_and_select_member_function {};
-
-struct use_single_agent_then_execute_with_nested_terminal_multi_agent_execute {};
-
-
-template<class Function>
-struct multi_agent_then_execute_returning_user_specified_container_functor
-{
-  mutable Function f;
-
-  template<class Index, class Container, class Arg>
-  __AGENCY_ANNOTATION
-  void operator()(const Index& idx, Container& c, Arg& arg) const
-  {
-    c[idx] = f(idx, arg);
-  }
-
-  template<class Index, class Container>
-  __AGENCY_ANNOTATION
-  void operator()(const Index& idx, Container& c) const
-  {
-    c[idx] = f(idx);
-  }
-};
-
-
-template<class Container, class Executor, class Function, class Future>
-using has_multi_agent_when_all_execute_and_select =
-  new_executor_traits_detail::has_multi_agent_when_all_execute_and_select<
-    Executor,
-    multi_agent_then_execute_returning_user_specified_container_functor<Function>,
-    detail::tuple<
-      typename new_executor_traits<Executor>::template future<Container>,
-      Future
-    >,
-    0
-  >;
-
+struct use_multi_agent_then_execute_with_shared_inits_returning_user_specified_container {};
 
 template<class Container, class Executor, class Function, class Future>
 using select_multi_agent_then_execute_returning_user_specified_container_implementation =
   typename std::conditional<
     has_multi_agent_then_execute_returning_user_specified_container<Container,Executor,Function,Future>::value,
     use_multi_agent_then_execute_returning_user_specified_container_member_function,
-    typename std::conditional<
-      has_multi_agent_when_all_execute_and_select<Container,Executor,Function,Future>::value,
-      use_multi_agent_when_all_execute_and_select_member_function,
-      use_single_agent_then_execute_with_nested_terminal_multi_agent_execute
-    >::type
+    use_multi_agent_then_execute_with_shared_inits_returning_user_specified_container
   >::type;
 
 
@@ -78,81 +37,62 @@ typename new_executor_traits<Executor>::template future<Container>
   multi_agent_then_execute_returning_user_specified_container(use_multi_agent_then_execute_returning_user_specified_container_member_function,
                                                               Executor& ex, Function f, typename new_executor_traits<Executor>::shape_type shape, Future& fut)
 {
-  return ex.template then_execute<Container>(ex, f, shape, fut);
+  return ex.template then_execute<Container>(f, shape, fut);
 } // end multi_agent_then_execute_returning_user_specified_container()
 
 
-template<class Container, class Executor, class Function, class Future>
-typename new_executor_traits<Executor>::template future<Container>
-  multi_agent_then_execute_returning_user_specified_container(use_multi_agent_when_all_execute_and_select_member_function,
-                                                              Executor& ex, Function f, typename new_executor_traits<Executor>::shape_type shape, Future& fut)
+template<class Function, class T>
+struct ignore_tail_parameters_and_invoke
 {
-  using traits = new_executor_traits<Executor>;
+  mutable Function f;
 
-  auto results = traits::template make_ready_future<Container>(ex, shape);
+  template<class Index, class... Args>
+  __AGENCY_ANNOTATION
+  typename std::result_of<Function(Index,T&)>::type
+  operator()(const Index& idx, T& past_arg, Args&&...) const
+  {
+    // XXX should use std::invoke
+    return f(idx, past_arg);
+  }
+};
 
-  auto results_and_fut = detail::make_tuple(std::move(results), std::move(fut));
 
-  return ex.template when_all_execute_and_select<0>(multi_agent_then_execute_returning_user_specified_container_functor<Function>{f}, shape, results_and_fut);
-} // end multi_agent_then_execute_returning_user_specified_container()
+template<class Function>
+struct ignore_tail_parameters_and_invoke<Function,void>
+{
+  mutable Function f;
+
+  template<class Index, class... Args>
+  __AGENCY_ANNOTATION
+  typename std::result_of<Function(Index)>::type
+  operator()(const Index& idx, Args&&...) const
+  {
+    // XXX should use std::invoke
+    return f(idx);
+  }
+};
 
 
-// overload for non-void futures
-template<class Container, class Executor, class Function, class Future>
+template<class Container, size_t... Indices, class Executor, class Function, class Future, class Tuple>
 typename new_executor_traits<Executor>::template future<Container>
-  multi_agent_then_execute_returning_user_specified_container_impl(Executor& ex, Function f, typename new_executor_traits<Executor>::shape_type shape, Future& fut,
-                                                                   typename std::enable_if<
-                                                                     !std::is_void<
-                                                                       typename future_traits<Future>::value_type
-                                                                     >::value
-                                                                   >::type* = 0)
+  multi_agent_then_execute_returning_user_specified_container_impl(detail::index_sequence<Indices...>,
+                                                                   Executor& ex, Function f, typename new_executor_traits<Executor>::shape_type shape, Future& fut,
+                                                                   const Tuple& tuple_of_ignored_parameters)
 {
   using value_type = typename future_traits<Future>::value_type;
 
-  return new_executor_traits<Executor>::then_execute(ex, [=, &ex](value_type& val)
-  {
-    using index_type = typename new_executor_traits<Executor>::index_type;
-
-    return new_executor_traits_detail::terminal_multi_agent_execute_returning_user_specified_container<Container>(ex, [=,&val](const index_type& idx)
-    {
-      return f(idx, val);
-    },
-    shape);
-  },
-  fut);
-} // end multi_agent_then_execute_returning_user_specified_container_impl()
-
-
-// overload for void futures
-template<class Container, class Executor, class Function, class Future>
-typename new_executor_traits<Executor>::template future<Container>
-  multi_agent_then_execute_returning_user_specified_container_impl(Executor& ex, Function f, typename new_executor_traits<Executor>::shape_type shape, Future& fut,
-                                                                   typename std::enable_if<
-                                                                     std::is_void<
-                                                                       typename future_traits<Future>::value_type
-                                                                     >::value
-                                                                   >::type* = 0)
-{
-  return new_executor_traits<Executor>::then_execute(ex, [=, &ex]
-  {
-    using index_type = typename new_executor_traits<Executor>::index_type;
-
-    return new_executor_traits_detail::terminal_multi_agent_execute_returning_user_specified_container<Container>(ex, [=](const index_type& idx)
-    {
-      return f(idx);
-    },
-    shape);
-  },
-  fut);
+  return new_executor_traits<Executor>::template then_execute<Container>(ex, ignore_tail_parameters_and_invoke<Function,value_type>{f}, shape, fut, std::get<Indices>(tuple_of_ignored_parameters)...);
 } // end multi_agent_then_execute_returning_user_specified_container_impl()
 
 
 template<class Container, class Executor, class Function, class Future>
 typename new_executor_traits<Executor>::template future<Container>
-  multi_agent_then_execute_returning_user_specified_container(use_single_agent_then_execute_with_nested_terminal_multi_agent_execute,
+  multi_agent_then_execute_returning_user_specified_container(use_multi_agent_then_execute_with_shared_inits_returning_user_specified_container,
                                                               Executor& ex, Function f, typename new_executor_traits<Executor>::shape_type shape, Future& fut)
 {
-  return multi_agent_then_execute_returning_user_specified_container_implementation_strategies::multi_agent_then_execute_returning_user_specified_container_impl<Container>(ex, f, shape, fut);
+  auto tuple_of_ignored_parameters = new_executor_traits_detail::make_tuple_of_ignored_parameters(ex);
+
+  return multi_agent_then_execute_returning_user_specified_container_impl<Container>(detail::make_index_sequence<std::tuple_size<decltype(tuple_of_ignored_parameters)>::value>(), ex, f, shape, fut, tuple_of_ignored_parameters);
 } // end multi_agent_then_execute_returning_user_specified_container()
 
 
@@ -176,9 +116,9 @@ typename new_executor_traits<Executor>::template future<Container>
 {
   namespace ns = detail::new_executor_traits_detail::multi_agent_then_execute_returning_user_specified_container_implementation_strategies;
 
-  using implementation_strategy = ns::select_multi_agent_then_execute_returning_user_specified_container_implementation<Container,Executor,Function,Future>;
+  using check_for_member_function = ns::select_multi_agent_then_execute_returning_user_specified_container_implementation<Container,Executor,Function,Future>;
 
-  return ns::multi_agent_then_execute_returning_user_specified_container<Container>(implementation_strategy(), ex, f, shape, fut);
+  return ns::multi_agent_then_execute_returning_user_specified_container<Container>(check_for_member_function(), ex, f, shape, fut);
 } // end new_executor_traits::then_execute()
 
 
