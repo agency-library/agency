615b6e4af1bb6e80a02a17d7c2eafe66eebde711
Jared Hoberock <jaredhoberock@gmail.com>
2015-07-10 17:05:31 -0700

    Minimize executor_traits.hpp such that it only contains stuff not yet found in new_executor_traits.hpp

--- agency/executor_traits.hpp
+++ agency/executor_traits.hpp
@@ -2,10 +2,6 @@
 
 #include <agency/future.hpp>
 #include <agency/detail/type_traits.hpp>
-#include <agency/detail/bind.hpp>
-#include <agency/execution_categories.hpp>
-#include <agency/detail/tuple.hpp>
-#include <agency/detail/shape_cast.hpp>
 #include <agency/new_executor_traits.hpp>
 
 
@@ -63,50 +59,23 @@ struct has_multi_agent_then_execute_impl;
 template<class Executor, class T, class... Types>
 struct has_multi_agent_then_execute_impl<Executor, T, type_list<Types...>>
 {
-  using index_type = typename new_executor_traits_detail::nested_index_type_with_default<
-    Executor
-  >::type;
-
-  using shape_type = typename new_executor_traits_detail::nested_shape_type_with_default<
-    Executor
-  >::type;
-
   template<class U>
   using future = typename nested_future_with_default<
     Executor
   >::template type_template<U>;
 
-  struct dummy_functor
-  {
-    void operator()(const index_type&, T&, Types&...) {}
-
-    // overload for void past parameter
-    void operator()(const index_type&, Types&...) {}
-  };
-
-  template<class Executor1,
-           class = decltype(
-             std::declval<Executor1>().then_execute(
-               std::declval<dummy_functor>(),
-               std::declval<shape_type>(),
-               *std::declval<future<T>*>(),
-               *std::declval<Types*>()...
-             )
-           )>
-  static std::true_type test(int);
-
-  template<class>
-  static std::false_type test(...);
-
-  using type = decltype(test<Executor>(0));
+  using type = new_executor_traits_detail::has_multi_agent_then_execute_with_shared_inits_returning_void<
+    Executor,
+    new_executor_traits_detail::test_function_returning_void,
+    future<void>,
+    Types...
+  >;
 };
 
 
-template<class T, bool = new_executor_traits_detail::has_execution_category<T>::value>
-struct has_multi_agent_then_execute : std::false_type {};
 
 template<class T>
-struct has_multi_agent_then_execute<T,true> 
+struct has_multi_agent_then_execute
   : has_multi_agent_then_execute_impl<
       T,
       int,
@@ -117,33 +86,10 @@ struct has_multi_agent_then_execute<T,true>
 {};
 
 
-template<class Executor, class T, class... Args>
-struct has_make_ready_future_impl
-{
-  template<
-    class Executor2,
-    typename = decltype(
-      std::declval<Executor2*>()->template make_ready_future<T>(
-        std::declval<Args>()...
-      )
-    )
-  >
-  static std::true_type test(int);
-
-  template<class>
-  static std::false_type test(...);
-
-  using type = decltype(test<Executor>(0));
-};
-
-
-template<class Executor, class T, class... Args>
-using has_make_ready_future = typename has_make_ready_future_impl<Executor,T,Args...>::type;
-
-
 } // end detail
 
 
+// XXX is_executor should be much more permissive and just check for any function supported by executor_traits
 template<class T>
 struct is_executor
   : std::integral_constant<
@@ -173,19 +119,6 @@ struct executor_index<Executor, typename std::enable_if<is_executor<Executor>::v
 
 template<class Executor>
 using executor_index_t = typename executor_index<Executor>::type;
-
-
-template<class Executor, class Enable = void>
-struct executor_shape {};
-
-template<class Executor>
-struct executor_shape<Executor, typename std::enable_if<is_executor<Executor>::value>::type>
-{
-  using type = typename executor_traits<Executor>::shape_type;
-};
-
-template<class Executor>
-using executor_shape_t = typename executor_shape<Executor>::type;
 
 
 template<class Executor, class T, class Enable = void>
