7a12e2ef1e8d5d2ac58fad71e5ae442dae1dccfa
Jared Hoberock <jaredhoberock@gmail.com>
2015-07-16 12:05:27 -0700

    Make this test program match the .cpp version

--- test_is_executor.cu
+++ test_is_executor.cu
@@ -10,16 +10,15 @@
 int main()
 {
   std::cout << "is_executor<grid_executor>: " << agency::is_executor<agency::cuda::grid_executor>::value << std::endl;
-  std::cout << "has_execution_category<grid_executor>: " << agency::detail::has_execution_category<agency::cuda::grid_executor>::value << std::endl;
-  std::cout << "has_then_execute<grid_executor>: " << agency::detail::has_then_execute<agency::cuda::grid_executor>::value << std::endl;
+  std::cout << "has_then_execute<grid_executor>: " << agency::detail::has_multi_agent_then_execute<agency::cuda::grid_executor>::value << std::endl;
 
   std::cout << "is_executor<cuda::parallel_executor>: "   << agency::is_executor<agency::cuda::parallel_executor>::value << std::endl;
 
   std::cout << "is_executor<cuda::concurrent_executor>: " << agency::is_executor<agency::cuda::concurrent_executor>::value << std::endl;
-  std::cout << "has_then_execute<cuda::concurrent_executor>: " << agency::detail::has_then_execute<agency::cuda::concurrent_executor>::value << std::endl;
+  std::cout << "has_then_execute<cuda::concurrent_executor>: " << agency::detail::has_multi_agent_then_execute<agency::cuda::concurrent_executor>::value << std::endl;
 
   std::cout << "is_executor<agency::nested_executor<agency::concurrent_executor,agency::sequential_executor>>: " << agency::is_executor<agency::nested_executor<agency::concurrent_executor,agency::sequential_executor>>::value << std::endl;
-  std::cout << "has_then_execute<agency::nested_executor<agency::concurrent_executor,agency::sequential_executor>>: " << agency::detail::has_then_execute<agency::nested_executor<agency::concurrent_executor,agency::sequential_executor>>::value << std::endl;
+  std::cout << "has_then_execute<agency::nested_executor<agency::concurrent_executor,agency::sequential_executor>>: " << agency::detail::has_multi_agent_then_execute<agency::nested_executor<agency::concurrent_executor,agency::sequential_executor>>::value << std::endl;
 
   return 0;
 }
