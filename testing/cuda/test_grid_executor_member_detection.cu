ead36b49794c5df50f9d2b5e1a98e4d78669b7d3
Jared Hoberock <jaredhoberock@gmail.com>
2015-08-10 17:21:49 -0700

    Detect multi-agent then_execute<Container> with shared inits

--- testing/cuda/test_grid_executor_member_detection.cu
+++ testing/cuda/test_grid_executor_member_detection.cu
@@ -2,6 +2,16 @@
 #include <agency/executor_traits.hpp>
 #include <iostream>
 
+struct functor_returning_int
+{
+  template<class... Args>
+  __host__ __device__
+  int operator()(Args&&...) const
+  {
+    return 0;
+  }
+};
+
 struct functor_returning_void
 {
   template<class T>
@@ -12,6 +22,20 @@ struct functor_returning_void
   void operator()() const {}
 };
 
+
+template<class T>
+struct container
+{
+  template<class Shape>
+  __host__ __device__
+  container(const Shape&);
+
+  template<class Index>
+  __host__ __device__
+  T& operator[](const Index&);
+};
+
+
 int main()
 {
   using executor_type = agency::cuda::grid_executor;
@@ -21,6 +45,11 @@ int main()
   static_assert(
     agency::detail::new_executor_traits_detail::has_multi_agent_then_execute_returning_void<executor_type, functor_returning_void, int_future_type>::value,
     "grid_executor should have multi-agent then_execute() returning void"
+  );
+
+  static_assert(
+    agency::detail::new_executor_traits_detail::has_multi_agent_then_execute_with_shared_inits_returning_user_specified_container<container<int>, executor_type, functor_returning_int, int_future_type, int, int>::value,
+    "grid_executor should have multi-agent then_execute() with shared inits returning user-specified container"
   );
 
   std::cout << "OK" << std::endl;
