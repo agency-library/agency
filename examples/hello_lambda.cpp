5936cbbe90fd358552e6caf19aff824ddff8e8a8
Jared Hoberock <jaredhoberock@gmail.com>
2015-10-06 13:59:44 -0500

    Add hello_lambda.cpp example program

---
+++ examples/hello_lambda.cpp
@@ -0,0 +1,14 @@
+#include <agency/execution_policy.hpp>
+#include <iostream>
+
+int main()
+{
+  // create 10 sequential_agents to greet us in bulk
+  agency::bulk_invoke(agency::seq(10), [](agency::sequential_agent& self)
+  {
+    std::cout << "Hello, world from agent " << self.index() << std::endl;
+  });
+
+  return 0;
+}
+
