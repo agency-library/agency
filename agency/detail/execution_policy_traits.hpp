51f32fe7ec2ea6ed7912b15819bf1630f4e5af8e
Jared Hoberock <jaredhoberock@gmail.com>
2015-10-28 14:13:10 -0500

    Add detail::execution_policy_executor & detail::execution_policy_executor_t

--- agency/detail/execution_policy_traits.hpp
+++ agency/detail/execution_policy_traits.hpp
@@ -10,14 +10,27 @@ namespace detail
 
 __DEFINE_HAS_NESTED_TYPE(has_executor_type, executor_type);
 
+
+template<class ExecutionPolicy, class Enable = void>
+struct execution_policy_executor {};
+
+template<class ExecutionPolicy>
+struct execution_policy_executor<ExecutionPolicy, typename std::enable_if<has_executor_type<ExecutionPolicy>::value>::type>
+{
+  using type = typename ExecutionPolicy::executor_type;
+};
+
+template<class ExecutionPolicy>
+using execution_policy_executor_t = typename execution_policy_executor<ExecutionPolicy>::type;
+
+
 template<class ExecutionPolicy, class T, class Enable = void>
 struct policy_future {};
 
-// gets the type of future bulk_async(policy) returns
 template<class ExecutionPolicy, class T>
 struct policy_future<ExecutionPolicy,T,typename std::enable_if<has_executor_type<ExecutionPolicy>::value>::type>
 {
-  using type = executor_future_t<typename ExecutionPolicy::executor_type, T>;
+  using type = executor_future_t<execution_policy_executor_t<ExecutionPolicy>, T>;
 };
 
 template<class ExecutionPolicy, class T>
@@ -25,6 +38,7 @@ using policy_future_t = typename policy_future<ExecutionPolicy,T>::type;
 
 
 __DEFINE_HAS_NESTED_TYPE(has_execution_agent_type, execution_agent_type);
+
 
 template<class ExecutionPolicy, class Enable = void>
 struct execution_policy_agent {};
