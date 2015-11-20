db4a351e151277e8e3be9235eef6228f5503cac8
Jared Hoberock <jaredhoberock@gmail.com>
2015-11-20 15:45:05 -0600

    Add programs which test that bulk_invoke & bulk_async can correctly return results
    Fixes #103

---
+++ test_bulk_invoke_results.cpp
@@ -0,0 +1,78 @@
+#include <agency/execution_policy.hpp>
+#include <agency/sequential_executor.hpp>
+#include <iostream>
+
+template<class ExecutionPolicy>
+void test()
+{
+  using execution_policy_type = ExecutionPolicy;
+
+  {
+    // bulk_invoke with no parameters
+    
+    execution_policy_type policy;
+
+    auto result = agency::bulk_invoke(policy(10),
+      [](typename execution_policy_type::execution_agent_type& self)
+    {
+      return 7;
+    });
+
+    using executor_type = typename ExecutionPolicy::executor_type;
+    using container_type = typename agency::executor_traits<executor_type>::template container<int>;
+
+    assert(container_type(10,7) == result);
+  }
+
+  {
+    // bulk_invoke with one parameter
+    
+    execution_policy_type policy;
+
+    int val = 13;
+
+    auto result = agency::bulk_invoke(policy(10),
+      [](typename execution_policy_type::execution_agent_type& self, int val)
+    {
+      return val;
+    },
+    val);
+
+    using executor_type = typename ExecutionPolicy::executor_type;
+    using container_type = typename agency::executor_traits<executor_type>::template container<int>;
+
+    assert(container_type(10,val) == result);
+  }
+
+  {
+    // bulk_invoke with one shared parameter
+    
+    execution_policy_type policy;
+
+    int val = 13;
+
+    auto result = agency::bulk_invoke(policy(10),
+      [](typename execution_policy_type::execution_agent_type& self, int& val)
+    {
+      return val;
+    },
+    agency::share<0>(val));
+
+    using executor_type = typename ExecutionPolicy::executor_type;
+    using container_type = typename agency::executor_traits<executor_type>::template container<int>;
+
+    assert(container_type(10,val) == result);
+  }
+}
+
+int main()
+{
+  test<agency::sequential_execution_policy>();
+  test<agency::concurrent_execution_policy>();
+  test<agency::parallel_execution_policy>();
+
+  std::cout << "OK" << std::endl;
+
+  return 0;
+}
+
