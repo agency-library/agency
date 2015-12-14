54372affe05f2d8e4796219de6972ac9c8ceaaa7
Jared Hoberock <jaredhoberock@gmail.com>
2015-12-14 11:52:06 -0600

    Update optional implementation

--- agency/detail/optional.hpp
+++ agency/detail/optional.hpp
@@ -1,8 +1,37 @@
+// Copyright (c) 2015, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #pragma once
 
 #include <agency/detail/config.hpp>
 #include <utility>
 #include <type_traits>
+#include <stdexcept>
+#include <initializer_list>
+#include <functional>
 
 namespace agency
 {
@@ -13,19 +42,41 @@ namespace detail
 struct nullopt_t {};
 constexpr nullopt_t nullopt{};
 
+struct in_place_t {};
+constexpr in_place_t in_place{};
+
+
+class bad_optional_access : public std::logic_error
+{
+  public:
+    explicit bad_optional_access(const std::string& what_arg) : logic_error(what_arg) {}
+    explicit bad_optional_access(const char* what_arg) : logic_error(what_arg) {}
+};
+
 
 namespace optional_detail
 {
 
 
-__agency_hd_warning_disable__
-template<class T1, class T2>
 __AGENCY_ANNOTATION
-static void swap(T1& a, T2& b)
+inline void throw_bad_optional_access(const char* what_arg)
 {
-  T1 tmp = std::move(a);
-  a = std::move(b);
-  b = std::move(tmp);
+#ifdef __CUDA_ARCH__
+  printf("bad_optional_access: %s\n", what_arg);
+  assert(0);
+#else
+  throw bad_optional_access(what_arg);
+#endif
+}
+
+
+#pragma nv_exec_check_disable
+template<class T>
+__AGENCY_ANNOTATION
+static void optional_swap(T& a, T& b)
+{
+  using std::swap;
+  swap(a,b);
 }
 
 
@@ -50,7 +101,7 @@ template<class T>
 struct optional_base<T,true> : T {};
 
 
-} // end optional_detail
+} // end detail
 
 
 template<class T>
@@ -89,9 +140,27 @@ class optional : public optional_detail::optional_base<T>
 
     __AGENCY_ANNOTATION
     optional(T&& value)
+      : optional(in_place, std::forward<T>(value))
+    {
+    }
+
+    template<class... Args>
+    __AGENCY_ANNOTATION
+    optional(in_place_t, Args&&... args)
       : contains_value_(false)
     {
-      emplace(std::move(value));
+      emplace(std::forward<Args>(args)...);
+    }
+
+    template<class U, class... Args,
+             class = typename std::enable_if<
+               std::is_constructible<T, std::initializer_list<U>&, Args&&...>::value
+             >::type>
+    __AGENCY_ANNOTATION
+    optional(in_place_t, std::initializer_list<U> ilist, Args&&... args)
+      : contains_value_(false)
+    {
+      emplace(ilist, std::forward<Args>(args)...);
     }
 
     __AGENCY_ANNOTATION
@@ -104,25 +173,6 @@ class optional : public optional_detail::optional_base<T>
     optional& operator=(nullopt_t)
     {
       clear();
-      return *this;
-    }
-
-    template<class U,
-             class = typename std::enable_if<
-               std::is_same<typename std::decay<U>::type,T>::value
-             >::type>
-    __AGENCY_ANNOTATION
-    optional& operator=(U&& value)
-    {
-      if(*this)
-      {
-        **this = std::forward<U>(value);
-      }
-      else
-      {
-        emplace(std::forward<U>(value));
-      }
-
       return *this;
     }
 
@@ -156,13 +206,45 @@ class optional : public optional_detail::optional_base<T>
       return *this;
     }
 
+    template<class U,
+             class = typename std::enable_if<
+               std::is_same<typename std::decay<U>::type,T>::value
+             >::type>
+    __AGENCY_ANNOTATION
+    optional& operator=(U&& value)
+    {
+      if(*this)
+      {
+        **this = std::forward<U>(value);
+      }
+      else
+      {
+        emplace(std::forward<U>(value));
+      }
+
+      return *this;
+    }
+
     template<class... Args>
     __AGENCY_ANNOTATION
     void emplace(Args&&... args)
     {
       clear();
 
-      new (operator->()) T(std::forward<Args>(args)...) ;
+      new (operator->()) T(std::forward<Args>(args)...);
+      contains_value_ = true;
+    }
+
+    template<class U, class... Args,
+             class = typename std::enable_if<
+               std::is_constructible<T,std::initializer_list<U>&, Args&&...>::value
+             >::type>
+    __AGENCY_ANNOTATION
+    void emplace(std::initializer_list<U> ilist, Args&&... args)
+    {
+      clear();
+
+      new (operator->()) T(ilist, std::forward<Args>(args)...);
       contains_value_ = true;
     }
 
@@ -175,22 +257,59 @@ class optional : public optional_detail::optional_base<T>
     __AGENCY_ANNOTATION
     T& value() &
     {
-      // XXX should check contains_value_ and throw otherwise
-      return operator*();
+      if(!*this)
+      {
+        optional_detail::throw_bad_optional_access("optional::value(): optional does not contain a value");
+      }
+
+      return **this;
     }
 
     __AGENCY_ANNOTATION
     const T& value() const &
     {
-      // XXX should check contains_value_ and throw otherwise
-      return operator*();
+      if(!*this)
+      {
+        optional_detail::throw_bad_optional_access("optional::value(): optional does not contain a value");
+      }
+
+      return **this;
     }
 
     __AGENCY_ANNOTATION
     T&& value() &&
     {
-      // XXX should check contains_value_ and throw otherwise
-      return std::move(operator*());
+      if(!*this)
+      {
+        optional_detail::throw_bad_optional_access("optional::value(): optional does not contain a value");
+      }
+
+      return std::move(**this);
+    }
+
+    __AGENCY_ANNOTATION
+    const T&& value() const &&
+    {
+      if(!*this)
+      {
+        optional_detail::throw_bad_optional_access("optional::value(): optional does not contain a value");
+      }
+
+      return std::move(**this);
+    }
+
+    template<class U>
+    __AGENCY_ANNOTATION
+    T value_or(U&& default_value) const &
+    {
+      return bool(*this) ? **this : static_cast<T>(std::forward<U>(default_value));
+    }
+
+    template<class U>
+    __AGENCY_ANNOTATION
+    T value_or(U&& default_value) &&
+    {
+      return bool(*this) ? std::move(**this) : static_cast<T>(std::forward<U>(default_value));
     }
 
     __AGENCY_ANNOTATION
@@ -200,9 +319,7 @@ class optional : public optional_detail::optional_base<T>
       {
         if(*this)
         {
-          using optional_detail::swap;
-
-          swap(**this, *other);
+          optional_detail::optional_swap(**this, *other);
         }
         else
         {
@@ -266,13 +383,125 @@ class optional : public optional_detail::optional_base<T>
     {
       if(*this)
       {
-        operator*().~T();
+        (**this).~T();
         contains_value_ = false;
       }
     }
 
     bool contains_value_;
 };
+
+
+// comparison
+template<class T>
+__AGENCY_ANNOTATION
+bool operator==(const optional<T>& lhs, const optional<T>& rhs)
+{
+  if(lhs && rhs)
+  {
+    return *lhs == *rhs;
+  }
+
+  return bool(lhs) == bool(rhs);
+}
+
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator<(const optional<T>& lhs, const optional<T>& rhs)
+{
+  if(lhs && rhs)
+  {
+    return *lhs < *rhs;
+  }
+
+  return !bool(lhs) && bool(rhs);
+}
+
+
+// comparison with nullopt_t
+template<class T>
+__AGENCY_ANNOTATION
+bool operator==(const optional<T>& lhs, nullopt_t)
+{
+  return lhs == optional<T>(nullopt);
+}
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator==(nullopt_t, const optional<T>& rhs)
+{
+  return optional<T>(nullopt) == rhs;
+}
+
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator<(const optional<T>& lhs, nullopt_t)
+{
+  return lhs < optional<T>(nullopt);
+}
+
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator<(nullopt_t, const optional<T>& rhs)
+{
+  return optional<T>(nullopt) < rhs;
+}
+
+
+// comparison with T
+template<class T>
+__AGENCY_ANNOTATION
+bool operator==(const optional<T>& lhs, const T& rhs)
+{
+  if(lhs)
+  {
+    return *lhs == rhs;
+  }
+
+  return false;
+}
+
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator==(const T& lhs, const optional<T>& rhs)
+{
+  if(rhs)
+  {
+    return lhs == *rhs;
+  }
+
+  return false;
+}
+
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator<(const optional<T>& lhs, const T& rhs)
+{
+  if(lhs)
+  {
+    return *lhs < rhs;
+  }
+
+  return true;
+}
+
+template<class T>
+__AGENCY_ANNOTATION
+bool operator<(const T& lhs, const optional<T>& rhs)
+{
+  if(rhs)
+  {
+    return lhs < *rhs;
+  }
+
+  return false;
+}
+
 
 
 template<class T>
