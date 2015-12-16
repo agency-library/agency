5abc5863e12db64f7b0c977944e36176ea0dea9e
Jared Hoberock <jaredhoberock@gmail.com>
2015-12-16 17:58:55 -0600

    Add agency::cuda::detail::boxed_value.hpp

---
+++ agency/cuda/detail/boxed_value.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <agency/detail/config.hpp>
+#include <agency/cuda/detail/allocator.hpp>
+#include <agency/detail/boxed_value.hpp>
+
+namespace agency
+{
+namespace cuda
+{
+namespace detail
+{
+
+
+template<class T>
+using boxed_value = agency::detail::boxed_value<T,allocator<T>>;
+
+
+} // end detail
+} // end cuda
+} // end agency
+
