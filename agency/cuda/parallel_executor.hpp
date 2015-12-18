35a90b6613c7a02c0d21d3cbfe60641433e5d186
Jared Hoberock <jaredhoberock@gmail.com>
2015-12-18 17:15:13 -0600

    Introduce cuda::this_thread::executor Add test_executor_array.cu test program

--- agency/cuda/parallel_executor.hpp
+++ agency/cuda/parallel_executor.hpp
@@ -2,11 +2,38 @@
 
 #include <agency/cuda/grid_executor.hpp>
 #include <agency/flattened_executor.hpp>
+#include <agency/cuda/detail/allocator.hpp>
+#include <agency/cuda/detail/pinned_allocator.hpp>
+#include <agency/cuda/detail/array.hpp>
+#include "uber_future.hpp"
 
 namespace agency
 {
 namespace cuda
 {
+namespace this_thread
+{
+
+
+class parallel_executor
+{
+  public:
+    using execution_category = parallel_execution_tag;
+
+    template<class T>
+    using allocator = cuda::detail::allocator<T, cuda::detail::pinned_allocator<T>>;
+
+    template<class T>
+    using container = cuda::detail::array<T, size_t, allocator<T>>;
+
+    //template<class T>
+    //using future = uber_future<T>;
+    template<class T>
+    using future = deferred_future<T>;
+};
+
+
+} // end this_thread
 
 
 using parallel_executor = agency::flattened_executor<grid_executor>;
