68d2e302378030993db7deea345a619e053ecd17
Jared Hoberock <jaredhoberock@gmail.com>
2015-12-18 16:36:35 -0600

    Introduce cuda::detail::pinned_allocator

---
+++ agency/cuda/detail/pinned_allocator.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <agency/detail/config.hpp>
+#include <cuda_runtime.h>
+
+namespace agency
+{
+namespace cuda
+{
+namespace detail
+{
+
+
+template<class T>
+class pinned_allocator
+{
+  public:
+    using value_type = T;
+
+    value_type* allocate(size_t n)
+    {
+      value_type* result = nullptr;
+
+      cudaError_t error = cudaMallocHost(reinterpret_cast<void**>(&result), n * sizeof(value_type));
+
+      if(error != cudaSuccess)
+      {
+        throw thrust::system_error(error, thrust::cuda_category(), "pinned_allocator::allocate(): cudaMallocHost");
+      }
+
+      return result;
+    }
+
+    void deallocate(value_type* ptr, size_t n)
+    {
+      cudaError_t error = cudaFreeHost(ptr);
+
+      if(error != cudaSuccess)
+      {
+        throw thrust::system_error(error, thrust::cuda_category(), "pinned_allocator::deallocate(): cudaFree");
+      }
+    }
+
+    template<class U, class... Args>
+    void construct(U* ptr, Args&&... args)
+    {
+      cudaError_t error = cudaDeviceSynchronize();
+
+      if(error != cudaSuccess)
+      {
+        throw thrust::system_error(error, thrust::cuda_category(), "pinned_allocator::allocate(): cudaDeviceSynchronize");
+      }
+
+      new(ptr) T(std::forward<Args>(args)...);
+    }
+};
+
+
+} // end detail
+} // end cuda
+} // end agency
+
