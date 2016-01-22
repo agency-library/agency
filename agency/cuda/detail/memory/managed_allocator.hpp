a5231af32de0f679ce4ca3af592af96f996fffac
Jared Hoberock <jaredhoberock@gmail.com>
2016-01-22 13:25:30 -0600

    Associate a device with managed_allocator Synchronize the entire system within managed_allocator::construct()

--- agency/cuda/detail/memory/managed_allocator.hpp
+++ agency/cuda/detail/memory/managed_allocator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <agency/detail/config.hpp>
+#include <agency/cuda/device.hpp>
 #include <thrust/system_error.h>
 #include <thrust/system/cuda/error.h>
 #include <cuda_runtime.h>
@@ -16,11 +17,33 @@ namespace detail
 template<class T>
 class managed_allocator
 {
+  private:
+    device_id device_;
+
+    const device_id& device() const
+    {
+      return device_;
+    }
+
   public:
     using value_type = T;
+
+    managed_allocator()
+      : device_()
+    {
+      auto devices = all_devices();
+
+      if(!devices.empty())
+      {
+        device_ = devices[0];
+      }
+    }
   
     value_type* allocate(size_t n)
     {
+      // switch to our device
+      scoped_current_device set_current_device(device());
+
       value_type* result = nullptr;
   
       cudaError_t error = cudaMallocManaged(&result, n * sizeof(T), cudaMemAttachGlobal);
@@ -35,6 +58,9 @@ class managed_allocator
   
     void deallocate(value_type* ptr, size_t)
     {
+      // switch to our device
+      scoped_current_device set_current_device(device());
+
       cudaError_t error = cudaFree(ptr);
   
       if(error != cudaSuccess)
@@ -46,12 +72,8 @@ class managed_allocator
     template<class U, class... Args>
     void construct(U* ptr, Args&&... args)
     {
-      cudaError_t error = cudaDeviceSynchronize();
-
-      if(error != cudaSuccess)
-      {
-        throw thrust::system_error(error, thrust::cuda_category(), "managed_allocator::allocate(): cudaDeviceSynchronize");
-      }
+      // we need to synchronize with all devices before touching the ptr
+      detail::wait(all_devices());
 
       new(ptr) T(std::forward<Args>(args)...);
     }
