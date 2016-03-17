8dc2a2ac73a94348177b2017787584667ffb5da8
Jared Hoberock <jaredhoberock@gmail.com>
2016-03-17 01:23:52 -0500

    Introduce cuda::detail::device_allocator

---
+++ agency/cuda/detail/memory/device_allocator.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <agency/detail/config.hpp>
+#include <agency/cuda/device.hpp>
+#include <thrust/system_error.h>
+#include <thrust/system/cuda/error.h>
+#include <cuda_runtime.h>
+
+namespace agency
+{
+namespace cuda
+{
+namespace detail
+{
+
+
+template<class T>
+class device_allocator
+{
+  private:
+    template<class> friend class device_allocator;
+
+    device_id device_;
+
+    const device_id& device() const
+    {
+      return device_;
+    }
+
+  public:
+    using value_type = T;
+
+    explicit device_allocator(const device_id& d)
+      : device_(d)
+    {}
+
+    device_allocator()
+      : device_allocator(all_devices()[0])
+    {}
+
+    device_allocator(const device_allocator&) = default;
+
+    template<class U>
+    device_allocator(const device_allocator<U>& other)
+      : device_(other.device_)
+    {}
+  
+    value_type* allocate(size_t n)
+    {
+      // switch to our device
+      scoped_current_device set_current_device(device());
+
+      value_type* result = nullptr;
+  
+      cudaError_t error = cudaMalloc(&result, n * sizeof(T));
+  
+      if(error != cudaSuccess)
+      {
+        throw thrust::system_error(error, thrust::cuda_category(), "device_allocator::allocate(): cudaMallocManaged");
+      }
+  
+      return result;
+    }
+  
+    void deallocate(value_type* ptr, size_t)
+    {
+      // switch to our device
+      scoped_current_device set_current_device(device());
+
+      cudaError_t error = cudaFree(ptr);
+  
+      if(error != cudaSuccess)
+      {
+        throw thrust::system_error(error, thrust::cuda_category(), "device_allocator::deallocate(): cudaFree");
+      }
+    }
+
+    template<class U, class... Args>
+    void construct(U* ptr, Args&&... args)
+    {
+      //new(ptr) U(std::forward<Args>(args)...);
+    }
+};
+
+
+} // end detail
+} // end cuda
+} // end agency
+
