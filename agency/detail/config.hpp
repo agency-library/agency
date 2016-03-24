6ab0963c53c62176c42ec41feac32934fd1821f7
Jared Hoberock <jaredhoberock@gmail.com>
2016-03-24 16:04:49 -0500

    Eliminate __AGENCY_CONSTEXPR_AFTER_CPP11

--- agency/detail/config.hpp
+++ agency/detail/config.hpp
@@ -1,15 +1,9 @@
 #pragma once
 
-#if __cplusplus > 201103L
-#define __AGENCY_CONSTEXPR_AFTER_CXX11 constexpr
-#else
-#define __AGENCY_CONSTEXPR_AFTER_CXX11
-#endif
-
 #ifdef __CUDACC__
 #  define __AGENCY_ANNOTATION __host__ __device__
 #else
-#  define __AGENCY_ANNOTATION __AGENCY_CONSTEXPR_AFTER_CXX11
+#  define __AGENCY_ANNOTATION
 #endif // __AGENCY_ANNOTATION
 
 #if defined(__NVCC__) && !(defined(__clang__) && defined(__CUDA__))
