24da8dcd507a46bb0d2dd0be7bc7c1e993e0f17a
Jared Hoberock <jaredhoberock@gmail.com>
2016-03-24 19:02:50 -0500

    Reorganize agency::this_thread::parallel_executor to allow its use without bringing in all of <agency/parallel_executor.hpp>

--- agency/parallel_executor.hpp
+++ agency/parallel_executor.hpp
@@ -1,24 +1,13 @@
 #pragma once
 
+#include <agency/detail/config.hpp>
 #include <agency/concurrent_executor.hpp>
-#include <agency/sequential_executor.hpp>
 #include <agency/nested_executor.hpp>
 #include <agency/flattened_executor.hpp>
+#include <agency/detail/this_thread_parallel_executor.hpp>
 
 namespace agency
 {
-namespace this_thread
-{
-
-
-class parallel_executor : public sequential_executor
-{
-  public:
-    using execution_category = parallel_execution_tag;
-};
-
-
-} // end this_thread
 
 
 using parallel_executor = flattened_executor<nested_executor<concurrent_executor, this_thread::parallel_executor>>;
