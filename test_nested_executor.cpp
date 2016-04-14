caecdbf3e838e17c580639941fed4f88d40c694b
Jared Hoberock <jaredhoberock@gmail.com>
2016-04-14 18:47:21 -0500

    Reenable some commented-out tests

--- test_nested_executor.cpp
+++ test_nested_executor.cpp
@@ -4,79 +4,106 @@
 #include <agency/bulk_invoke.hpp>
 #include <iostream>
 #include <thread>
+#include <tuple>
 
 std::mutex mut;
 
 int main()
 {
-  agency::nested_executor<agency::concurrent_executor, agency::sequential_executor> ex;
+  // XXX we should really assert that the results match the expected value
 
-  agency::bulk_async(ex, std::make_pair(2,2), [](std::tuple<size_t,size_t> idx)
   {
-    mut.lock();
-    std::cout << "Hello world from thread " << std::this_thread::get_id() << std::endl;
-    std::cout << "Hello world from index " << std::get<0>(idx) << ", " << std::get<1>(idx) << std::endl;
-    mut.unlock();
-  }).wait();
+    using executor_type = agency::nested_executor<agency::concurrent_executor, agency::sequential_executor>;
+    executor_type ex;
 
-  std::cout << std::endl;
+    using traits = agency::executor_traits<executor_type>;
 
-//  // test with shared variables
-//  ex.async_execute([](std::tuple<size_t,size_t> idx, int& shared0, int& shared1)
-//  {
-//    mut.lock();
-//    if(std::get<0>(idx) == 0 && std::get<1>(idx) == 0)
-//    {
-//      // set the first shared variable to 7
-//      shared0 = 7;
-//    }
-//    mut.unlock();
-//
-//    mut.lock();
-//    std::cout << "Hello world from thread " << std::this_thread::get_id() << std::endl;
-//    std::cout << "Hello world from index " << std::get<0>(idx) << ", " << std::get<1>(idx) << std::endl;
-//    std::cout << "1st shared variable: " << shared0 << std::endl;
-//    std::cout << "2nd shared variable: " << shared1 << std::endl;
-//    mut.unlock();
-//
-//    // increment 2nd shared variable
-//    ++shared1;
-//  },
-//  std::make_pair(2,2),
-//  13, 0
-//  ).wait();
-//
-//  std::cout << std::endl;
-//
-//  // test with 3-deep nesting
-//  agency::nested_executor<agency::sequential_executor, agency::nested_executor<agency::sequential_executor,agency::sequential_executor>> ex2;
-//
-//  ex2.async_execute([](std::tuple<size_t,size_t,size_t> idx)
-//  {
-//    std::cout << "(" << std::get<0>(idx) << ", " << std::get<1>(idx) << ", " << std::get<2>(idx) << ")" << std::endl;
-//  },
-//  std::make_tuple(2,2,2)
-//  ).wait();
-//
-//  std::cout << std::endl;
-//
-//  // test with shared variables
-//  ex2.async_execute([](std::tuple<size_t,size_t,size_t> idx, int& shared0, int& shared1, int& shared2)
-//  {
-//    std::cout << "(" << std::get<0>(idx) << ", " << std::get<1>(idx) << ", " << std::get<2>(idx) << ")" << std::endl;
-//    std::cout << "1st shared variable: " << shared0 << std::endl;
-//    std::cout << "2nd shared variable: " << shared1 << std::endl;
-//    std::cout << "3rd shared variable: " << shared2 << std::endl;
-//
-//    // increment shared variables
-//    shared0 += 1;
-//    shared1 += 2;
-//    shared2 += 3;
-//  },
-//  std::make_tuple(2,2,2),
-//  13, 0, 7
-//  ).wait();
+    using index_type = traits::index_type;
+    using shape_type = traits::shape_type;
 
+    traits::async_execute(ex, 
+      [](index_type idx)
+      {
+        mut.lock();
+        std::cout << "Hello world from thread " << std::this_thread::get_id() << std::endl;
+        std::cout << "Hello world from index " << std::get<0>(idx) << ", " << std::get<1>(idx) << std::endl;
+        mut.unlock();
+      },
+      shape_type(2,2)
+    ).wait();
+
+    std::cout << std::endl;
+
+    // test with shared variables
+    traits::async_execute(ex,
+      [](index_type idx, int& shared0, int& shared1)
+      {
+        mut.lock();
+        if(std::get<0>(idx) == 0 && std::get<1>(idx) == 0)
+        {
+          // set the first shared variable to 7
+          shared0 = 7;
+        }
+        mut.unlock();
+
+        mut.lock();
+        std::cout << "Hello world from thread " << std::this_thread::get_id() << std::endl;
+        std::cout << "Hello world from index " << std::get<0>(idx) << ", " << std::get<1>(idx) << std::endl;
+        std::cout << "1st shared variable: " << shared0 << std::endl;
+        std::cout << "2nd shared variable: " << shared1 << std::endl;
+        mut.unlock();
+
+        // increment 2nd shared variable
+        ++shared1;
+      },
+      shape_type(2,2),
+      []{ return 13; },
+      []{ return 0; }
+    ).wait();
+
+    std::cout << std::endl;
+  }
+
+  {
+    // test with 3-deep nesting
+    
+    using executor_type = agency::nested_executor<agency::sequential_executor, agency::nested_executor<agency::sequential_executor,agency::sequential_executor>>;
+    using traits = agency::executor_traits<executor_type>;
+    executor_type ex;
+
+    using index_type = traits::index_type;
+    using shape_type = traits::shape_type;
+
+    traits::async_execute(ex,
+      [](index_type idx)
+      {
+        std::cout << "(" << std::get<0>(idx) << ", " << std::get<1>(idx) << ", " << std::get<2>(idx) << ")" << std::endl;
+      },
+      shape_type(2,2,2)
+    ).wait();
+
+    std::cout << std::endl;
+
+    // test with shared variables
+    traits::async_execute(ex,
+      [](index_type idx, int& shared0, int& shared1, int& shared2)
+      {
+        std::cout << "(" << std::get<0>(idx) << ", " << std::get<1>(idx) << ", " << std::get<2>(idx) << ")" << std::endl;
+        std::cout << "1st shared variable: " << shared0 << std::endl;
+        std::cout << "2nd shared variable: " << shared1 << std::endl;
+        std::cout << "3rd shared variable: " << shared2 << std::endl;
+
+        // increment shared variables
+        shared0 += 1;
+        shared1 += 2;
+        shared2 += 3;
+      },
+      shape_type(2,2,2),
+      []{ return 13; },
+      []{ return 0; },
+      []{ return 7; }
+    ).wait();
+  }
 
   return 0;
 }
