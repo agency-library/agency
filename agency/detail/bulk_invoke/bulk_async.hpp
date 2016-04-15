e855971ed775b87fa6ccb6f62345975f386cc7a2
Jared Hoberock <jaredhoberock@gmail.com>
2016-04-14 19:08:10 -0500

    Disable agency::bulk_async() as an customization point Eliminate some unnecessary enable_ifs related to bulk_async()

--- agency/detail/bulk_invoke/bulk_async.hpp
+++ agency/detail/bulk_invoke/bulk_async.hpp
@@ -10,25 +10,6 @@ namespace detail
 {
 
 
-// detect whether the expression bulk_async(args...) is well-formed
-template<class... Args>
-struct has_bulk_async_impl
-{
-  template<class... UArgs,
-           class = decltype(bulk_async(std::declval<UArgs>()...))
-          >
-  static std::true_type test(int);
-
-  template<class...>
-  static std::false_type test(...);
-
-  using type = decltype(test<Args...>(0));
-};
-
-template<class... Args>
-using has_bulk_async = typename has_bulk_async_impl<Args...>::type;
-
-
 // this overload handles the general case where the user function returns a normal result
 template<class Executor, class Function, class Factory, class Tuple, size_t... TupleIndices>
 executor_future_t<Executor, typename std::result_of<Factory(executor_shape_t<Executor>)>::type>
@@ -86,32 +67,8 @@ template<class Executor, class Function, class... Args>
 using bulk_async_executor_result_t = typename bulk_async_executor_result<Executor,Function,Args...>::type;
 
 
-template<bool enable, class Executor, class Function, class... Args>
-struct enable_if_bulk_async_executor_impl {};
-
 template<class Executor, class Function, class... Args>
-struct enable_if_bulk_async_executor_impl<
-         true, Executor, Function, Args...
-       >
-  : enable_if_call_possible<
-      bulk_async_executor_result_t<Executor,Function,Args...>,
-      Function,
-      executor_index_t<Executor>,
-      decay_parameter_t<Args>...
-    >
-{};
-
-template<class Executor, class Function, class... Args>
-struct enable_if_bulk_async_executor
-  : enable_if_bulk_async_executor_impl<
-      is_executor<Executor>::value, Executor, Function, Args...
-    >
-{};
-
-
-// XXX we no longer need this enable_if
-template<class Executor, class Function, class... Args>
-typename detail::enable_if_bulk_async_executor<Executor, Function, Args...>::type
+bulk_async_executor_result_t<Executor, Function, Args...>
   bulk_async_executor(Executor& exec, typename executor_traits<typename std::decay<Executor>::type>::shape_type shape, Function f, Args&&... args)
 {
   // the _1 is for the executor idx parameter, which is the first parameter passed to f
@@ -206,13 +163,10 @@ bulk_async_execution_policy_result_t<
 }
 
 
-// generic implementation of bulk_async() for execution policies
-// only enable it if there is not already some other implementation
-template<class ExecutionPolicy, class Function, class... Args,
-         class = typename std::enable_if<
-           !has_bulk_async<ExecutionPolicy&&,Function,Args&&...>::value
-         >::type
-        >
+} // end detail
+
+
+template<class ExecutionPolicy, class Function, class... Args>
 typename detail::enable_if_bulk_async_execution_policy<
   ExecutionPolicy, Function, Args...
 >::type
@@ -223,31 +177,6 @@ typename detail::enable_if_bulk_async_execution_policy<
 
   return detail::bulk_async_execution_policy_impl(detail::index_sequence_for<Args...>(), detail::make_index_sequence<num_shared_params>(), policy, f, std::forward<Args>(args)...);
 }
-
-
-struct call_bulk_async_via_adl
-{
-  // XXX put enable_ifs on this?
-  template<class ExecutionPolicy, class Function, class... Args>
-  auto operator()(ExecutionPolicy&& policy, Function f, Args&&... args) const ->
-    decltype(bulk_async(std::forward<ExecutionPolicy>(policy), f, std::forward<Args>(args)...))
-  {
-    return bulk_async(std::forward<ExecutionPolicy>(policy), f, std::forward<Args>(args)...);
-  }
-};
-
-
-} // end detail
-
-
-//namespace
-//{
-
-
-constexpr const detail::call_bulk_async_via_adl  bulk_async{};
-
-
-//} // end anon namespace
 
 
 } // end agency
