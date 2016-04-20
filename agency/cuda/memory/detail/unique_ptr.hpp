5169322bdc33947737448b504ff3ffe0a16277cd
Jared Hoberock <jaredhoberock@gmail.com>
2016-04-20 14:11:07 -0500

    Introduce agency::cuda::detail::release_and_delete_when()

--- agency/cuda/memory/detail/unique_ptr.hpp
+++ agency/cuda/memory/detail/unique_ptr.hpp
@@ -3,6 +3,7 @@
 #include <agency/detail/config.hpp>
 #include <agency/detail/unique_ptr.hpp>
 #include <agency/cuda/memory/allocator.hpp>
+#include <agency/cuda/detail/future/event.hpp>
 
 namespace agency
 {
@@ -21,6 +22,30 @@ __host__ __device__
 unique_ptr<T> make_unique(Args&&... args)
 {
   return agency::detail::allocate_unique<T>(allocator<T>(),std::forward<Args>(args)...);
+}
+
+
+template<class T, class Deleter>
+struct release_and_delete_when_functor
+{
+  T* ptr;
+  mutable Deleter deleter;
+
+  __host__ __device__
+  void operator()() const
+  {
+    deleter(ptr);
+  }
+};
+
+
+template<class T>
+__host__ __device__
+void release_and_delete_when(unique_ptr<T>& ptr, event& e)
+{
+  auto continuation = release_and_delete_when_functor<T,typename unique_ptr<T>::deleter_type>{ptr.release(), ptr.get_deleter()};
+
+  e.then(continuation);
 }
 
 
