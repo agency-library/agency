e47833eeb74f912a18d11ae95345000f59185f2c
Jared Hoberock <jaredhoberock@gmail.com>
2016-04-20 14:11:41 -0500

    Introduce agency::cuda::detail::invalidate_and_destroy_when() Introduce agency::cuda::detail::async_invalidate_and_destroy()

--- agency/cuda/detail/future/asynchronous_state.hpp
+++ agency/cuda/detail/future/asynchronous_state.hpp
@@ -3,6 +3,8 @@
 #include <agency/detail/config.hpp>
 #include <agency/detail/asynchronous_state.hpp>
 #include <agency/cuda/memory/allocator.hpp>
+#include <agency/cuda/memory/detail/unique_ptr.hpp>
+#include <type_traits>
 
 namespace agency
 {
@@ -14,6 +16,46 @@ namespace detail
 
 template<class T>
 using asynchronous_state = agency::detail::asynchronous_state<T,allocator<T>>;
+
+
+// XXX it might be better for the following functions to be members of asynchronous_state
+//     it also might better to have asynchronous_state's destructor asynchronously destroy the contained state
+//     that way, we wouldn't have to make special arrangements inside of clients of asynchronous_state
+
+template<class T>
+__host__ __device__
+typename std::enable_if<
+  std::is_void<typename asynchronous_state<T>::storage_type>::value
+>::type
+  invalidate_and_destroy_when(asynchronous_state<T>& state, event&)
+{
+  // assign an default-constructed state to invalidate it
+  state = asynchronous_state<T>();
+
+  // destruction is a no-op
+}
+
+
+template<class T>
+__host__ __device__
+typename std::enable_if<
+  !std::is_void<typename asynchronous_state<T>::storage_type>::value
+>::type
+  invalidate_and_destroy_when(asynchronous_state<T>& state, event& e)
+{
+  // releasing the state's storage implicitly invalidates it
+  agency::cuda::detail::release_and_delete_when(state.storage(), e);
+}
+
+
+template<class T>
+__host__ __device__
+void async_invalidate_and_destroy(asynchronous_state<T>& state)
+{
+  auto immediately_ready = detail::make_ready_event();
+
+  agency::cuda::detail::invalidate_and_destroy_when(state, immediately_ready);
+}
 
   
 } // end detail
