9724d87bb2a1409d442cb88cca38d69725ef0afb
Jared Hoberock <jaredhoberock@gmail.com>
2016-05-06 17:17:06 -0500

    Introduce agency.hpp and cuda.hpp
    Fixes #192

---
+++ agency/cuda.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <agency/detail/config.hpp>
+#include <agency/cuda/device.hpp>
+#include <agency/cuda/execution_agent.hpp>
+#include <agency/cuda/execution_policy.hpp>
+#include <agency/cuda/executor.hpp>
+#include <agency/cuda/future.hpp>
+#include <agency/cuda/memory.hpp>
+
