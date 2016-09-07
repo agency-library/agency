e515812fc4e577416958de0c114e5bb34f099491
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-07 14:51:10 -0500

    Add a macro for a pseudo Concept shorthand

---
+++ agency/detail/requires.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <agency/detail/config.hpp>
+#include <type_traits>
+
+// The __AGENCY_REQUIRES() macro may be used in a function template's parameter list
+// to simulate Concepts.
+//
+// For example, to selectively enable a function template only for integer types,
+// we could do something like this:
+//
+//     template<class Integer,
+//              __AGENCY_REQUIRES(std::is_integral<Integer>::value)
+//             >
+//     Integer plus_one(Integer x)
+//     {
+//       return x + 1;
+//     }
+//
+#define __AGENCY_REQUIRES(...) typename std::enable_if<__VA_ARGS__>::type* = nullptr
+
