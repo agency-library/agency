1170a5842b7615ecc391dc0aaba0a421c9ef7c45
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-08 14:46:43 -0500

    Add sequenced_executor::bulk_execute() Add unit test for sequenced_executor

--- agency/execution/executor/sequenced_executor.hpp
+++ agency/execution/executor/sequenced_executor.hpp
@@ -14,6 +14,21 @@ class sequenced_executor
   public:
     using execution_category = sequenced_execution_tag;
 
+    template<class Function, class ResultFactory, class SharedFactory>
+    agency::detail::result_of_t<ResultFactory()>
+      bulk_execute(Function f, size_t n, ResultFactory result_factory, SharedFactory shared_factory)
+    {
+      auto result = result_factory();
+      auto shared_parm = shared_factory();
+
+      for(size_t i = 0; i < n; ++i)
+      {
+        f(i, result, shared_parm);
+      }
+
+      return std::move(result);
+    }
+
     template<class Function, class Factory>
     __AGENCY_ANNOTATION
     void execute(Function f, size_t n, Factory shared_factory)
