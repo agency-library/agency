cc88c5a88441480e0aa5183eca9d2727fe11c86d
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-19 17:24:39 -0500

    Renable use of __AGENCY_REQUIRES in bulk_then_execute_with_void_result()

--- agency/execution/executor/detail/new_executor_traits/bulk_then_execute_with_void_result.hpp
+++ agency/execution/executor/detail/new_executor_traits/bulk_then_execute_with_void_result.hpp
@@ -53,14 +53,13 @@ struct ignore_unit_result_parameter_and_invoke<Function,void>
 
 __agency_exec_check_disable__
 template<class E, class Function, class Future, class... Factories,
-         __AGENCY_REQUIRES(BulkExecutor<E>())
-         //__AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
+         __AGENCY_REQUIRES(BulkExecutor<E>()),
+         __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
         >
 __AGENCY_ANNOTATION
 executor_future_t<E,void>
   bulk_then_execute_with_void_result(E& exec, Function f, executor_shape_t<E> shape, Future& predecessor, Factories... factories)
 {
-  static_assert(sizeof...(Factories) == executor_execution_depth<E>::value, "got wrong number of factories");
   using predecessor_type = future_value_t<Future>;
 
   // wrap f in a functor that will ignore the unit object we pass to it
