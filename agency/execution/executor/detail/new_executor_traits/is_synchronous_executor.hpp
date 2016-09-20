8ab8acbc60713c5feb05c027f9593f0059455046
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-20 18:06:44 -0500

    Workaround an nvcc 8.0 bug in the implementation of is_synchronous_executor

--- agency/execution/executor/detail/new_executor_traits/is_synchronous_executor.hpp
+++ agency/execution/executor/detail/new_executor_traits/is_synchronous_executor.hpp
@@ -2,6 +2,9 @@
 
 #include <agency/detail/config.hpp>
 #include <agency/detail/type_traits.hpp>
+#include <type_traits>
+#include <utility>
+
 
 namespace agency
 {
@@ -11,16 +14,58 @@ namespace new_executor_traits_detail
 {
 
 
-template<class T, class Function>
-using sync_execute_t = decltype(std::declval<T>().sync_execute(std::declval<Function>()));
+template<class Executor, class Function>
+struct has_sync_execute_impl
+{
+  using expected_result_type = result_of_t<Function()>;
+
+  template<class Executor1,
+           class ReturnType = decltype(
+             std::declval<Executor1>().sync_execute(
+               std::declval<Function>()
+             )
+           ),
+           class = typename std::enable_if<
+             std::is_same<ReturnType,expected_result_type>::value
+           >::type>
+  static std::true_type test(int);
+
+  template<class>
+  static std::false_type test(...);
+
+  using type = decltype(test<Executor>(0));
+};
 
 
-template<class T, class Function>
-using has_sync_execute = is_detected_exact<result_of_t<Function()>, sync_execute_t, T, Function>;
+template<class Executor, class Function>
+using has_sync_execute = typename has_sync_execute_impl<Executor, Function>::type;
 
 
 template<class T>
-using is_synchronous_executor = has_sync_execute<T, std::function<void()>>;
+struct is_synchronous_executor_impl
+{
+  // types related to functions passed to .sync_execute()
+  using result_type = int;
+
+  // the function we'll pass to .sync_execute() to test
+
+  // XXX WAR nvcc 8.0 bug
+  //using test_function = std::function<result_type()>;
+
+  struct test_function
+  {
+    result_type operator()();
+  };
+
+  using type = has_sync_execute<
+    T,
+    test_function
+  >;
+};
+
+
+template<class T>
+using is_synchronous_executor = typename is_synchronous_executor_impl<T>::type;
 
 
 // a fake Concept to use with __AGENCY_REQUIRES
