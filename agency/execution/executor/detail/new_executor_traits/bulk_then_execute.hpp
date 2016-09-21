9bb1c1d3ab4787bcbf3fcdb203f9b08c10b983c3
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-21 14:49:28 -0500

    Implement bulk_then_execute() with predecessor.then() + exec.bulk_execute()

--- agency/execution/executor/detail/new_executor_traits/bulk_then_execute.hpp
+++ agency/execution/executor/detail/new_executor_traits/bulk_then_execute.hpp
@@ -10,7 +10,6 @@
 #include <agency/execution/executor/detail/new_executor_traits/executor_shape.hpp>
 #include <agency/execution/executor/detail/new_executor_traits/executor_execution_depth.hpp>
 #include <agency/execution/executor/detail/new_executor_traits/bulk_then_execute.hpp>
-#include <agency/execution/executor/detail/new_executor_traits/bulk_async_execute.hpp>
 #include <future>
 
 
@@ -37,6 +36,10 @@ bulk_then_execute(E& exec, Function f, executor_shape_t<E> shape, Future& predec
 {
   return exec.bulk_then_execute(f, shape, predecessor, result_factory, shared_factories...);
 }
+
+
+namespace bulk_then_execute_detail
+{
 
 
 template<class Function, class SharedFuture,
@@ -109,6 +112,8 @@ struct bulk_then_execute_functor<Function,SharedFuture,true>
 };
 
 
+} // end bulk_then_execute_detail
+
 
 // this case handles executors which have .bulk_async_execute() and may or may not have .bulk_execute()
 __agency_exec_check_disable__
@@ -123,14 +128,92 @@ executor_future_t<
 >
 bulk_then_execute(E& exec, Function f, executor_shape_t<E> shape, Future& predecessor, ResultFactory result_factory, Factories... shared_factories)
 {
+  using namespace bulk_then_execute_detail;
+
   // XXX we may wish to allow the executor to participate in this sharing operation
   auto shared_predecessor_future = future_traits<Future>::share(predecessor);
 
   using shared_predecessor_future_type = decltype(shared_predecessor_future);
   auto functor = bulk_then_execute_functor<Function,shared_predecessor_future_type>{f, shared_predecessor_future};
 
-  return bulk_async_execute(exec, functor, shape, result_factory, shared_factories...);
+  return exec.bulk_async_execute(functor, shape, result_factory, shared_factories...);
 }
+
+
+namespace bulk_then_execute_detail
+{
+
+
+// this functor is used by the implementation of bulk_then_execute() below which calls .then() with a nested bulk_execute() inside
+// this definition is for the general case, when the predecessor Future type is non-void
+template<class Executor, class Function, class Predecessor, class ResultFactory, class... SharedFactories>
+struct then_with_nested_bulk_execute_functor
+{
+  mutable Executor exec;
+  mutable Function f;
+  executor_shape_t<Executor> shape;
+  mutable ResultFactory result_factory;
+  mutable detail::tuple<SharedFactories...> shared_factories;
+
+  // this functor is passed to bulk_execute() below
+  // it has a reference to the predecessor future to use as a parameter to f
+  struct functor_for_bulk_execute
+  {
+    mutable Function f;
+    Predecessor& predecessor;
+
+    template<class Index, class Result, class... SharedArgs>
+    __AGENCY_ANNOTATION
+    void operator()(const Index& idx, Result& result, SharedArgs&... shared_args) const
+    {
+      agency::detail::invoke(f, idx, predecessor, result, shared_args...);
+    }
+  };
+
+  template<size_t... Indices>
+  __AGENCY_ANNOTATION
+  result_of_t<ResultFactory()> impl(detail::index_sequence<Indices...>, Predecessor& predecessor) const
+  {
+    functor_for_bulk_execute functor{f, predecessor};
+
+    return exec.bulk_execute(functor, shape, result_factory, detail::get<Indices>(shared_factories)...);
+  }
+
+  __AGENCY_ANNOTATION
+  result_of_t<ResultFactory()> operator()(Predecessor& predecessor) const
+  {
+    return impl(detail::make_index_sequence<sizeof...(SharedFactories)>(), predecessor);
+  }
+};
+
+
+// this specialization is for the case when the predecessor Future type is void
+template<class Executor, class Function, class ResultFactory, class... SharedFactories>
+struct then_with_nested_bulk_execute_functor<Executor,Function,void,ResultFactory,SharedFactories...>
+{
+  mutable Executor exec;
+  mutable Function f;
+  executor_shape_t<Executor> shape;
+  mutable ResultFactory result_factory;
+  mutable detail::tuple<SharedFactories...> shared_factories;
+
+  template<size_t... Indices>
+  __AGENCY_ANNOTATION
+  result_of_t<ResultFactory()> impl(detail::index_sequence<Indices...>) const
+  {
+    return exec.bulk_execute(f, shape, result_factory, detail::get<Indices>(shared_factories)...);
+  }
+
+  // the predecessor future is void, so operator() receives no parameter
+  __AGENCY_ANNOTATION
+  result_of_t<ResultFactory()> operator()() const
+  {
+    return impl(detail::make_index_sequence<sizeof...(SharedFactories)>());
+  }
+};
+
+
+} // end bulk_then_execute_detail
 
 
 // this case handles executors which only have .bulk_execute()
@@ -146,21 +229,16 @@ executor_future_t<
 >
 bulk_then_execute(E& exec, Function f, executor_shape_t<E> shape, Future& predecessor, ResultFactory result_factory, Factories... shared_factories)
 {
-  // XXX we may wish to allow the executor to participate in this sharing operation
-  auto shared_predecessor_future = future_traits<Future>::share(predecessor);
+  using namespace bulk_then_execute_detail;
 
-  // XXX we should call async_execute(exec, ...) instead of std::async() here
-  // XXX alternatively, we could call then_execute(exec, predecessor, ...) and not wait inside the function
-  
-  // XXX need to use a __host__ __device__ functor here instead of a lambda
+  using predecessor_type = future_value_t<Future>;
+  then_with_nested_bulk_execute_functor<E,Function,predecessor_type,ResultFactory,Factories...> functor{exec,f,shape,result_factory,detail::make_tuple(shared_factories...)};
 
-  return std::async(std::launch::deferred, [=]() mutable
-  {
-    using shared_predecessor_future_type = decltype(shared_predecessor_future);
-    auto functor = bulk_then_execute_functor<Function,shared_predecessor_future_type>{f, shared_predecessor_future};
+  auto intermediate_fut = future_traits<Future>::then(predecessor, std::move(functor));
+  using result_type = result_of_t<ResultFactory()>;
 
-    return bulk_execute(exec, functor, shape, result_factory, shared_factories...);
-  });
+  // XXX we may wish to allow the executor to participate in this cast operation
+  return future_traits<decltype(intermediate_fut)>::template cast<result_type>(intermediate_fut);
 }
 
 
