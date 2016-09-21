d882e8a1fd07a3658d5dfa0d56610cace046199d
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-21 17:41:31 -0500

    Implement cuda::this_thread::parallel_executor::bulk_execute()

--- agency/cuda/execution/executor/parallel_executor.hpp
+++ agency/cuda/execution/executor/parallel_executor.hpp
@@ -29,6 +29,22 @@ class parallel_executor
 
     template<class T>
     using future = cuda::future<T>;
+
+    template<class Function, class ResultFactory, class SharedFactory>
+    __AGENCY_ANNOTATION
+    agency::detail::result_of_t<ResultFactory()>
+      bulk_execute(Function f, size_t n, ResultFactory result_factory, SharedFactory shared_factory)
+    {
+      auto result = result_factory();
+      auto shared_arg = shared_factory();
+
+      for(size_t i = 0; i < n; ++i)
+      {
+        f(i, result, shared_arg);
+      }
+
+      return std::move(result);
+    }
 };
 
 
