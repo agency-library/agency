bbe3ce17e7736dc557dbae95ebf65d28f33ba4e8
Jared Hoberock <jaredhoberock@gmail.com>
2016-09-23 15:36:38 -0500

    Introduce agency::new_executor_execution_category and unit tests

---
+++ agency/execution/executor/new_executor_traits/executor_execution_category.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <agency/detail/config.hpp>
+#include <agency/execution/executor/new_executor_traits/is_bulk_executor.hpp>
+#include <agency/execution/executor/new_executor_traits/detail/member_execution_category_or.hpp>
+#include <agency/execution/execution_categories.hpp>
+
+namespace agency
+{
+namespace detail
+{
+
+
+template<class BulkExecutor, bool Enable = is_bulk_executor<BulkExecutor>::value>
+struct executor_execution_category_impl
+{
+};
+
+template<class BulkExecutor>
+struct executor_execution_category_impl<BulkExecutor,true>
+{
+  using type = member_execution_category_or_t<BulkExecutor,unsequenced_execution_tag>;
+};
+
+
+} // end detail
+
+
+template<class BulkExecutor>
+struct new_executor_execution_category : detail::executor_execution_category_impl<BulkExecutor> {};
+
+template<class BulkExecutor>
+using new_executor_execution_category_t = typename new_executor_execution_category<BulkExecutor>::type;
+
+
+} // end agency
+
