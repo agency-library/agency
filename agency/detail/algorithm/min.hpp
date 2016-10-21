0c26ba1d1d3047ba19dd03ab5af6055e0871bffc
Jared Hoberock <jaredhoberock@gmail.com>
2016-10-21 17:51:59 -0500

    Properly annotate detail::min & detail::max

--- agency/detail/algorithm/min.hpp
+++ agency/detail/algorithm/min.hpp
@@ -8,13 +8,18 @@ namespace detail
 {
 
 
+__agency_exec_check_disable__
 template<class T>
+__AGENCY_ANNOTATION
 const T& min(const T& a, const T& b)
 {
   return b < a ? b : a;
 }
 
+
+__agency_exec_check_disable__
 template<class T, class Compare>
+__AGENCY_ANNOTATION
 const T& min(const T& a, const T& b, Compare comp)
 {
   return comp(b, a) ? b : a;
