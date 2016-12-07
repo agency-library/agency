ae2d1086e4c41cf6d20c2c5db6d5a16ae11b1d3b
Jared Hoberock <jaredhoberock@gmail.com>
2016-12-07 15:37:16 -0600

    Introduce two-dimensional execution policies
    Fixes #296

--- testing/unorganized/multidimensional_agent.cpp
+++ testing/unorganized/multidimensional_agent.cpp
@@ -1,11 +1,9 @@
 #include <agency/agency.hpp>
 #include <iostream>
 
-const agency::basic_execution_policy<agency::parallel_agent_2d, agency::parallel_executor> par2d{};
-
 int main()
 {
-  auto exec = par2d({0,0}, {2,2});
+  auto exec = agency::par2d({0,0}, {2,2});
 
   agency::bulk_invoke(exec, [](agency::parallel_agent_2d& self)
   {
