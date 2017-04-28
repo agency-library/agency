3bc6cfe4002808834e817fa9f429fbfbee28aaff
Jared Hoberock <jaredhoberock@gmail.com>
2017-04-28 15:10:56 -0500

    Introduce dynamic_execution_tag Introduce detail::common_execution_category

--- agency/execution/execution_categories.hpp
+++ agency/execution/execution_categories.hpp
@@ -27,6 +27,9 @@
 
 #pragma once
 
+#include <agency/detail/config.hpp>
+#include <agency/detail/type_traits.hpp>
+
 #include <type_traits>
 #include <stddef.h>
 
@@ -50,13 +53,18 @@ struct parallel_execution_tag {};
 /// \ingroup execution_categories
 struct unsequenced_execution_tag {};
 
-
+/// \brief Type representing the scoped execution category.
+/// \ingroup execution_categories
 template<class ExecutionCategory1, class ExecutionCategory2>
 struct scoped_execution_tag
 {
   using outer_execution_category = ExecutionCategory1;
   using inner_execution_category = ExecutionCategory2;
 };
+
+/// \brief Type representing the dynamic execution category.
+/// \ingroup execution_categories
+struct dynamic_execution_tag {};
 
 
 namespace detail
@@ -76,10 +84,11 @@ namespace detail
 // parallel_execution_tag    < sequenced_execution_tag
 // parallel_execution_tag    < concurrent_execution_tag
 // unsequenced_execution_tag < parallel_execution_tag
+// dynamic_execution_tag     < unsequenced_execution_tag
 //
 // XXX figure out how scoped_execution_tag sorts
 
-// most categories are not weaker than another
+// in general, categories are not weaker than another
 template<class ExecutionCategory1, class ExecutionCategory2>
 struct is_weaker_than : std::false_type {};
 
@@ -87,7 +96,15 @@ struct is_weaker_than : std::false_type {};
 template<class ExecutionCategory>
 struct is_weaker_than<ExecutionCategory,ExecutionCategory> : std::true_type {};
 
-// unsequenced is weaker than everything
+// dynamic is weaker than everything else
+template<class ExecutionCategory2>
+struct is_weaker_than<dynamic_execution_tag, ExecutionCategory2> : std::true_type {};
+
+// introduce this specialization to disambiguate two other specializations
+template<>
+struct is_weaker_than<dynamic_execution_tag, dynamic_execution_tag> : std::true_type {};
+
+// unsequenced is weaker than everything except dynamic
 template<class ExecutionCategory2>
 struct is_weaker_than<unsequenced_execution_tag, ExecutionCategory2> : std::true_type {};
 
@@ -95,16 +112,15 @@ struct is_weaker_than<unsequenced_execution_tag, ExecutionCategory2> : std::true
 template<>
 struct is_weaker_than<unsequenced_execution_tag, unsequenced_execution_tag> : std::true_type {};
 
-// parallel is weaker than everything except unsequenced
-template<class ExecutionCategory2>
-struct is_weaker_than<parallel_execution_tag, ExecutionCategory2> : std::true_type {};
-
-// introduce this specialization to disambiguate two other specializations
 template<>
-struct is_weaker_than<parallel_execution_tag, parallel_execution_tag> : std::true_type {};
+struct is_weaker_than<unsequenced_execution_tag, dynamic_execution_tag> : std::false_type {};
+
+// parallel is weaker than sequenced & concurrent
+template<>
+struct is_weaker_than<parallel_execution_tag, sequenced_execution_tag> : std::true_type {};
 
 template<>
-struct is_weaker_than<parallel_execution_tag, unsequenced_execution_tag> : std::false_type {};
+struct is_weaker_than<parallel_execution_tag, concurrent_execution_tag> : std::true_type {};
 
 
 template<class ExecutionCategory>
@@ -126,6 +142,128 @@ struct execution_depth<scoped_execution_tag<ExecutionCategory1,ExecutionCategory
       1 + execution_depth<ExecutionCategory2>::value
     >
 {};
+
+
+// this namespace contains the implementation of common_execution_category
+namespace common_execution_category_detail
+{
+
+
+// because the implementation of common_execution_category2 is recursive, we introduce
+// a forward declaration so it may call itself.
+template<class ExecutionCategory1, class ExecutionCategory2>
+struct common_execution_category2;
+
+template<class ExecutionCategory1, class ExecutionCategory2>
+using common_execution_category2_t = typename common_execution_category2<ExecutionCategory1,ExecutionCategory2>::type;
+
+
+// the implementation of common_execution_category2_impl is recursive, and there are two base cases
+
+// base case 1: the input categories have different depths
+// i.e., one may be "flat" and the other scoped,
+// or both may be scoped but have different depths
+template<class ExecutionCategory1, class ExecutionCategory2, size_t depth1, size_t depth2>
+struct common_execution_category2_impl
+{
+  // there's no commonality between the two input categories
+  // so the result is "dynamic" -- there are no static guarantees that can be provided
+  using type = dynamic_execution_tag;
+};
+
+
+// base case 2: both input categories are "flat"
+template<class ExecutionCategory1, class ExecutionCategory2>
+struct common_execution_category2_impl<ExecutionCategory1,ExecutionCategory2,1,1>
+{
+  // both ExecutionCategory1 & ExecutionCategory2 have depth 1 -- they are "flat"
+
+  // if one of the two categories is weaker than the other, then return it
+  // otherwise, return the weakest static guarantee: unsequenced
+  using type = conditional_t<
+    is_weaker_than<ExecutionCategory1,ExecutionCategory2>::value,
+    ExecutionCategory1,
+    conditional_t<
+      is_weaker_than<ExecutionCategory2,ExecutionCategory1>::value,
+      ExecutionCategory2,
+      unsequenced_execution_tag
+    >
+  >;
+};
+
+
+// recursive case: both input categories are scoped
+template<class OuterCategory1, class InnerCategory1, class OuterCategory2, class InnerCategory2, size_t depth>
+struct common_execution_category2_impl<
+  scoped_execution_tag<OuterCategory1,InnerCategory1>,
+  scoped_execution_tag<OuterCategory2,InnerCategory2>,
+  depth,depth
+>
+{
+  // both categories are scoped and they have the same depth
+  // XXX it may not matter so much that the depth is the same.
+  //     we may still be able to apply this recipe sensibly even
+  //     when the two input categories' depths differ
+
+  // the result is scoped. apply common_execution_category to
+  // the inputs' constituents to get the result's constituents
+  using type = scoped_execution_tag<
+    common_execution_category2_t<OuterCategory1,OuterCategory2>,
+    common_execution_category2_t<InnerCategory1,InnerCategory2>
+  >;
+};
+
+
+template<class ExecutionCategory1, class ExecutionCategory2>
+struct common_execution_category2
+{
+  using type = typename common_execution_category2_impl<
+    ExecutionCategory1,
+    ExecutionCategory2,
+    execution_depth<ExecutionCategory1>::value,
+    execution_depth<ExecutionCategory2>::value
+  >::type;
+};
+
+
+} // end common_execution_category_detail
+
+
+// common_execution_category is a type trait which, given one or more possibly different execution categories,
+// returns a category representing the strongest guarantees that can be made given the different input possibilities
+template<class ExecutionCategory, class... ExecutionCategories>
+struct common_execution_category;
+
+template<class ExecutionCategory, class... ExecutionCategories>
+using common_execution_category_t = typename common_execution_category<ExecutionCategory,ExecutionCategories...>::type;
+
+
+// the implementation of common_execution_category is recursive
+// this is the recursive case
+template<class ExecutionCategory1, class ExecutionCategory2, class... ExecutionCategories>
+struct common_execution_category<ExecutionCategory1, ExecutionCategory2, ExecutionCategories...>
+{
+  using type = common_execution_category_t<
+    ExecutionCategory1,
+    common_execution_category_t<ExecutionCategory2, ExecutionCategories...>
+  >;
+};
+
+// base case 1: a single category
+template<class ExecutionCategory>
+struct common_execution_category<ExecutionCategory>
+{
+  using type = ExecutionCategory;
+};
+
+// base case 2: two categories
+template<class ExecutionCategory1, class ExecutionCategory2>
+struct common_execution_category<ExecutionCategory1,ExecutionCategory2>
+{
+  // with two categories, we lower onto the two category
+  // implementation inside common_execution_category_detail
+  using type = common_execution_category_detail::common_execution_category2_t<ExecutionCategory1,ExecutionCategory2>;
+};
 
 
 } // end detail
