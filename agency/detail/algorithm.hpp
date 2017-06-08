fb9a1dc4603b0fc06dae8fbd257c5de66b7829d1
Jared Hoberock <jaredhoberock@gmail.com>
2017-06-08 15:06:42 -0500

    Add detail::destroy()

--- agency/detail/algorithm.hpp
+++ agency/detail/algorithm.hpp
@@ -4,6 +4,7 @@
 #include <agency/detail/algorithm/construct_n.hpp>
 #include <agency/detail/algorithm/copy.hpp>
 #include <agency/detail/algorithm/copy_n.hpp>
+#include <agency/detail/algorithm/destroy.hpp>
 #include <agency/detail/algorithm/equal.hpp>
 #include <agency/detail/algorithm/max.hpp>
 #include <agency/detail/algorithm/min.hpp>
