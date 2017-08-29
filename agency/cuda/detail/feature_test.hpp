29f542967636b14e48ceb2261cccb00f20f8679a
Jared Hoberock <jaredhoberock@gmail.com>
2017-08-29 15:43:50 -0500

    Tweak the definition of __cuda_lib_has_cooperative_groups

--- agency/cuda/detail/feature_test.hpp
+++ agency/cuda/detail/feature_test.hpp
@@ -29,7 +29,7 @@
 #endif
 
 
-#if (__CUDA_ARCH__ != 0) && (CUDART_VERSION >= 9000) && (__CUDACC_RDC__)
+#if (CUDART_VERSION >= 9000) && (__CUDACC_RDC__)
 #  define __cuda_lib_has_cooperative_groups 1
 #else
 #  define __cuda_lib_has_cooperative_groups 0
