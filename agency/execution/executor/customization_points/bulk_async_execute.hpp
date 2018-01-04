e10d7fead41f2183f65b68a6a1d5287b1c648fbf
Jared Hoberock <jaredhoberock@gmail.com>
2018-01-04 17:48:47 -0600

    Introduce bulk_twoway_executor and implement bulk_async_execute() in terms of it

--- agency/execution/executor/customization_points/bulk_async_execute.hpp
+++ agency/execution/executor/customization_points/bulk_async_execute.hpp
@@ -4,6 +4,7 @@
 #include <agency/detail/requires.hpp>
 #include <agency/future.hpp>
 #include <agency/execution/executor/customization_points/bulk_then_execute.hpp>
+#include <agency/execution/executor/detail/adaptors/bulk_twoway_executor.hpp>
 #include <agency/execution/executor/executor_traits.hpp>
 #include <agency/detail/type_traits.hpp>
 
@@ -11,9 +12,8 @@ namespace agency
 {
 
 
-__agency_exec_check_disable__
 template<class E, class Function, class ResultFactory, class... Factories,
-         __AGENCY_REQUIRES(detail::BulkAsynchronousExecutor<E>()),
+         __AGENCY_REQUIRES(detail::BulkAsynchronousExecutor<E>() or detail::BulkContinuationExecutor<E>()),
          __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
         >
 __AGENCY_ANNOTATION
@@ -23,13 +23,16 @@ executor_future_t<
 >
 bulk_async_execute(E& exec, Function f, executor_shape_t<E> shape, ResultFactory result_factory, Factories... shared_factories)
 {
-  return exec.bulk_async_execute(f, shape, result_factory, shared_factories...);
+  return detail::bulk_twoway_executor<E>(exec).bulk_twoway_execute(f, shape, result_factory, shared_factories...);
 }
 
 
+// XXX nomerge
+// XXX eliminate this when we eliminate .bulk_sync_execute()
 __agency_exec_check_disable__
 template<class E, class Function, class ResultFactory, class... Factories,
-         __AGENCY_REQUIRES(detail::BulkExecutor<E>() && !detail::BulkAsynchronousExecutor<E>()),
+         __AGENCY_REQUIRES(!detail::BulkAsynchronousExecutor<E>() and !detail::BulkContinuationExecutor<E>()),
+         __AGENCY_REQUIRES(detail::BulkSynchronousExecutor<E>()),
          __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
         >
 __AGENCY_ANNOTATION
