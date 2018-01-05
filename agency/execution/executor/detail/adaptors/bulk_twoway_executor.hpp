a21f9a9e72c5226c7e15c5e7bc63d03e72b01ec2
Jared Hoberock <jaredhoberock@gmail.com>
2018-01-05 14:38:26 -0600

    Eliminate superfluous #includes and add missing ones

--- agency/execution/executor/detail/adaptors/bulk_twoway_executor.hpp
+++ agency/execution/executor/detail/adaptors/bulk_twoway_executor.hpp
@@ -28,10 +28,10 @@
 
 #include <agency/detail/config.hpp>
 #include <agency/detail/requires.hpp>
-#include <agency/detail/invoke.hpp>
 #include <agency/detail/type_traits.hpp>
-#include <agency/execution/executor/executor_traits/executor_future.hpp>
 #include <agency/execution/executor/detail/adaptors/basic_executor_adaptor.hpp>
+#include <agency/execution/executor/executor_traits/executor_future.hpp>
+#include <agency/execution/executor/executor_traits/detail/is_bulk_twoway_executor.hpp>
 #include <agency/future.hpp>
 
 
