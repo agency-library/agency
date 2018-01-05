85e823eabcf24f5c18f684cf99664991d15bda75
Jared Hoberock <jaredhoberock@gmail.com>
2018-01-05 14:38:55 -0600

    Introduce detail::bulk_then_executor Implement bulk_then_execute() in terms of detail::bulk_then_executor

--- agency/execution/executor/customization_points/bulk_then_execute.hpp
+++ agency/execution/executor/customization_points/bulk_then_execute.hpp
@@ -2,21 +2,21 @@
 
 #include <agency/detail/config.hpp>
 #include <agency/detail/requires.hpp>
-#include <agency/detail/type_traits.hpp>
+#include <agency/execution/executor/detail/adaptors/bulk_then_executor.hpp>
 #include <agency/execution/executor/executor_traits.hpp>
+#include <agency/detail/type_traits.hpp>
 #include <agency/future/detail/future_cast.hpp>
 #include <agency/tuple.hpp>
-#include <future>
 
 
 namespace agency
 {
 
 
-// this case handles executors which have .bulk_then_execute()
+// this case handles executors which may be adapted by detail::bulk_then_executor
 __agency_exec_check_disable__
 template<class E, class Function, class Future, class ResultFactory, class... Factories,
-         __AGENCY_REQUIRES(detail::BulkContinuationExecutor<E>()),
+         __AGENCY_REQUIRES(detail::BulkAsynchronousExecutor<E>() or detail::BulkContinuationExecutor<E>()),
          __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
         >
 __AGENCY_ANNOTATION
@@ -26,115 +26,7 @@ executor_future_t<
 >
 bulk_then_execute(E& exec, Function f, executor_shape_t<E> shape, Future& predecessor, ResultFactory result_factory, Factories... shared_factories)
 {
-  return exec.bulk_then_execute(f, shape, predecessor, result_factory, shared_factories...);
-}
-
-
-namespace detail
-{
-
-
-template<class Function, class SharedFuture,
-         bool Enable = std::is_void<detail::future_value_t<SharedFuture>>::value
-        >
-struct bulk_then_execute_functor
-{
-  mutable Function f_;
-  mutable SharedFuture fut_;
-
-  using predecessor_type = typename future_traits<SharedFuture>::value_type;
-
-  __agency_exec_check_disable__
-  __AGENCY_ANNOTATION
-  ~bulk_then_execute_functor() = default;
-
-  __agency_exec_check_disable__
-  __AGENCY_ANNOTATION
-  bulk_then_execute_functor(Function f, const SharedFuture& fut)
-    : f_(f), fut_(fut)
-  {}
-
-  __agency_exec_check_disable__
-  __AGENCY_ANNOTATION
-  bulk_then_execute_functor(const bulk_then_execute_functor&) = default;
-
-  __agency_exec_check_disable__
-  template<class Index, class... Args>
-  __AGENCY_ANNOTATION
-  auto operator()(const Index &idx, Args&... args) const ->
-    decltype(f_(idx, const_cast<predecessor_type&>(fut_.get()),args...))
-  {
-    predecessor_type& predecessor = const_cast<predecessor_type&>(fut_.get());
-
-    return f_(idx, predecessor, args...);
-  }
-};
-
-
-template<class Function, class SharedFuture>
-struct bulk_then_execute_functor<Function,SharedFuture,true>
-{
-  mutable Function f_;
-  mutable SharedFuture fut_;
-
-  __agency_exec_check_disable__
-  __AGENCY_ANNOTATION
-  ~bulk_then_execute_functor() = default;
-
-  __agency_exec_check_disable__
-  __AGENCY_ANNOTATION
-  bulk_then_execute_functor(Function f, const SharedFuture& fut)
-    : f_(f), fut_(fut)
-  {}
-
-  __agency_exec_check_disable__
-  __AGENCY_ANNOTATION
-  bulk_then_execute_functor(const bulk_then_execute_functor&) = default;
-
-  __agency_exec_check_disable__
-  template<class Index, class... Args>
-  __AGENCY_ANNOTATION
-  auto operator()(const Index &idx, Args&... args) const ->
-    decltype(f_(idx, args...))
-  {
-    fut_.wait();
-
-    return f_(idx, args...);
-  }
-};
-
-
-template<class Function, class SharedFuture>
-__AGENCY_ANNOTATION
-bulk_then_execute_functor<Function,SharedFuture> make_bulk_then_execute_functor(Function f, const SharedFuture& shared_future)
-{
-  return bulk_then_execute_functor<Function,SharedFuture>(f, shared_future);
-}
-
-
-} // end detail
-
-
-
-// this case handles executors which have .bulk_async_execute() and may or may not have .bulk_sync_execute()
-__agency_exec_check_disable__
-template<class E, class Function, class Future, class ResultFactory, class... Factories,
-         __AGENCY_REQUIRES(!detail::BulkContinuationExecutor<E>() && detail::BulkAsynchronousExecutor<E>()),
-         __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
-        >
-__AGENCY_ANNOTATION
-executor_future_t<
-  E,
-  detail::result_of_t<ResultFactory()>
->
-bulk_then_execute(E& exec, Function f, executor_shape_t<E> shape, Future& predecessor, ResultFactory result_factory, Factories... shared_factories)
-{
-  // XXX we may wish to allow the executor to participate in this sharing operation
-  auto shared_predecessor_future = future_traits<Future>::share(predecessor);
-
-  auto functor = detail::make_bulk_then_execute_functor(f, shared_predecessor_future);
-
-  return exec.bulk_async_execute(functor, shape, result_factory, shared_factories...);
+  return detail::bulk_then_executor<E>(exec).bulk_then_execute(f, shape, predecessor, result_factory, shared_factories...);
 }
 
 
@@ -216,10 +108,12 @@ struct then_with_nested_bulk_sync_execute_functor<Executor,Function,void,ResultF
 } // end detail
 
 
-// this case handles executors which only have .bulk_sync_execute()
+// XXX nomerge
+// XXX eliminate this when we eliminate .bulk_sync_execute()
+// this case handles executors which only have .bulk_sync_execute() -- these may not be adapted by detail::bulk_then_executor
 __agency_exec_check_disable__
 template<class E, class Function, class Future, class ResultFactory, class... Factories,
-         __AGENCY_REQUIRES(!detail::BulkContinuationExecutor<E>() && !detail::BulkAsynchronousExecutor<E>()),
+         __AGENCY_REQUIRES(!detail::BulkAsynchronousExecutor<E>() and !detail::BulkContinuationExecutor<E>()),
          __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
         >
 __AGENCY_ANNOTATION
