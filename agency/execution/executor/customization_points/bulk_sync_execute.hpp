0ce21f6304ec21a2fcb8a99b3cf00c8e1b1c2ad3
Jared Hoberock <jaredhoberock@gmail.com>
2018-01-25 12:03:07 -0600

    Implement agency::bulk_sync_execute() via always_blocking_executor

--- agency/execution/executor/customization_points/bulk_sync_execute.hpp
+++ agency/execution/executor/customization_points/bulk_sync_execute.hpp
@@ -5,6 +5,7 @@
 #include <agency/detail/type_traits.hpp>
 #include <agency/execution/executor/executor_traits.hpp>
 #include <agency/execution/executor/customization_points/bulk_async_execute.hpp>
+#include <agency/execution/executor/detail/adaptors/always_blocking_executor.hpp>
 
 namespace agency
 {
@@ -25,14 +26,15 @@ bulk_sync_execute(E& exec, Function f, executor_shape_t<E> shape, ResultFactory 
 
 __agency_exec_check_disable__
 template<class E, class Function, class ResultFactory, class... Factories,
-         __AGENCY_REQUIRES(detail::BulkExecutor<E>() && !detail::BulkSynchronousExecutor<E>()),
+         __AGENCY_REQUIRES(is_executor<E>::value && !detail::BulkSynchronousExecutor<E>()),
          __AGENCY_REQUIRES(executor_execution_depth<E>::value == sizeof...(Factories))
         >
 __AGENCY_ANNOTATION
 detail::result_of_t<ResultFactory()>
 bulk_sync_execute(E& exec, Function f, executor_shape_t<E> shape, ResultFactory result_factory, Factories... shared_factories)
 {
-  return agency::bulk_async_execute(exec, f, shape, result_factory, shared_factories...).get();
+  detail::always_blocking_executor<E> blocking_exec(exec);
+  return agency::bulk_async_execute(blocking_exec, f, shape, result_factory, shared_factories...).get();
 }
 
 
