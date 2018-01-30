819557f095c5c50cb942401e62fd87b66b0aa261
Jared Hoberock <jaredhoberock@gmail.com>
2018-01-30 17:10:21 -0600

    Eliminate superfluous is_single_twoway_executor

--- agency/execution/executor/executor_traits/is_executor.hpp
+++ agency/execution/executor/executor_traits/is_executor.hpp
@@ -4,7 +4,6 @@
 #include <agency/detail/type_traits.hpp>
 #include <agency/execution/executor/executor_traits/is_simple_executor.hpp>
 #include <agency/execution/executor/executor_traits/is_bulk_executor.hpp>
-#include <agency/execution/executor/executor_traits/detail/is_single_twoway_executor.hpp>
 #include <agency/execution/executor/executor_traits/detail/is_bulk_then_executor.hpp>
 #include <agency/execution/executor/executor_traits/detail/is_bulk_twoway_executor.hpp>
 #include <agency/execution/executor/executor_traits/detail/is_then_executor.hpp>
@@ -18,7 +17,6 @@ template<class T>
 using is_executor = agency::detail::disjunction<
   is_simple_executor<T>,
   is_bulk_executor<T>,
-  detail::is_single_twoway_executor<T>,
   detail::is_bulk_then_executor<T>,
   detail::is_bulk_twoway_executor<T>,
   detail::is_then_executor<T>,
