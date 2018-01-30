5f5774cd2cf0604c145a65b5eef2222dff1bbf46
Jared Hoberock <jaredhoberock@gmail.com>
2018-01-30 14:45:21 -0600

    Expand the scope of is_bulk_executor to is_bulk_twoway_executor

--- agency/execution/executor/executor_traits/is_bulk_executor.hpp
+++ agency/execution/executor/executor_traits/is_bulk_executor.hpp
@@ -2,6 +2,7 @@
 
 #include <agency/detail/config.hpp>
 #include <agency/detail/type_traits.hpp>
+#include <agency/execution/executor/executor_traits/detail/is_bulk_twoway_executor.hpp>
 #include <agency/execution/executor/executor_traits/is_bulk_asynchronous_executor.hpp>
 #include <agency/execution/executor/executor_traits/is_bulk_continuation_executor.hpp>
 
@@ -12,7 +13,8 @@ namespace agency
 template<class T>
 using is_bulk_executor = agency::detail::disjunction<
   is_bulk_asynchronous_executor<T>,
-  is_bulk_continuation_executor<T>
+  is_bulk_continuation_executor<T>,
+  detail::is_bulk_twoway_executor<T>
 >;
 
 
