50a90009c901ca89c41e4922d6dadf7da3988b68
Jared Hoberock <jaredhoberock@gmail.com>
2018-02-12 15:45:38 -0600

    Use executor_ref within blocking_bulk_twoway_execute to avoid an unnecessary copy

--- agency/execution/executor/detail/utility/blocking_bulk_twoway_execute.hpp
+++ agency/execution/executor/detail/utility/blocking_bulk_twoway_execute.hpp
@@ -3,9 +3,12 @@
 #include <agency/detail/config.hpp>
 #include <agency/detail/requires.hpp>
 #include <agency/detail/type_traits.hpp>
-#include <agency/execution/executor/executor_traits.hpp>
-#include <agency/execution/executor/detail/utility/bulk_twoway_execute.hpp>
+#include <agency/execution/executor/executor_traits/executor_shape.hpp>
+#include <agency/execution/executor/executor_traits/executor_execution_depth.hpp>
+#include <agency/execution/executor/executor_traits/is_executor.hpp>
+#include <agency/execution/executor/detail/adaptors/executor_ref.hpp>
 #include <agency/execution/executor/detail/adaptors/always_blocking_executor.hpp>
+#include <agency/execution/executor/detail/utility/bulk_twoway_execute.hpp>
 
 namespace agency
 {
@@ -22,7 +25,13 @@ __AGENCY_ANNOTATION
 detail::result_of_t<ResultFactory()>
 blocking_bulk_twoway_execute(const E& exec, Function f, executor_shape_t<E> shape, ResultFactory result_factory, Factories... shared_factories)
 {
-  detail::always_blocking_executor<E> blocking_exec(exec);
+  // grab a reference to exec so that a copy is not created inside of always_blocking_executor
+  detail::executor_ref<E> exec_ref(exec);
+
+  // adapt exec_ref into an always blocking executor
+  detail::always_blocking_executor<executor_ref<E>> blocking_exec(exec_ref);
+
+  // do a bulk_twoway_exec using the blocking_exec
   return detail::bulk_twoway_execute(blocking_exec, f, shape, result_factory, shared_factories...).get();
 }
 
