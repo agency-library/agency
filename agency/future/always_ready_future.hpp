57061877b9462015ffd8241fb43135e1d9fc5523
Jared Hoberock <jaredhoberock@gmail.com>
2018-02-12 18:23:51 -0600

    Port always_ready_future for use with CUDA

--- agency/future/always_ready_future.hpp
+++ agency/future/always_ready_future.hpp
@@ -6,6 +6,9 @@
 #include <agency/detail/utility.hpp>
 #include <agency/experimental/variant.hpp>
 #include <agency/experimental/optional.hpp>
+#include <agency/future/future_traits/is_future.hpp>
+#include <agency/future/future_traits/future_value.hpp>
+#include <agency/detail/terminate.hpp>
 
 #include <exception>
 #include <type_traits>
@@ -23,20 +26,23 @@ namespace detail
 {
 
 
-// declare always_ready_then() for always_ready_future::then() below
+// declare always_ready_then_and_leave() for always_ready_future::then() below
 template<class T, class Function>
+__AGENCY_ANNOTATION
 always_ready_future<detail::result_of_t<Function&&(T&)>>
-  always_ready_then(always_ready_future<T>& future, Function&& f);
+  always_ready_then_and_leave_valid(always_ready_future<T>& future, Function&& f);
 
 template<class Function>
+__AGENCY_ANNOTATION
 always_ready_future<detail::result_of_t<Function&&()>>
-  always_ready_then(always_ready_future<void>& future, Function&& f);
+  always_ready_then_and_leave_valid(always_ready_future<void>& future, Function&& f);
 
 
 } // end detail
 
 
-// always_ready_future is a future that is always created in a ready state
+// always_ready_future is a future that is created in a ready state
+// this type of future is always ready as long as it is valid
 //
 // Executors which always block their client can use always_ready_future as their
 // associated future and still expose two-way asynchronous execution functions like twoway_execute()
@@ -45,18 +51,34 @@ template<class T>
 class always_ready_future
 {
   public:
+    __AGENCY_ANNOTATION
     always_ready_future(const T& value) : state_(value) {}
 
+    __AGENCY_ANNOTATION
     always_ready_future(T&& value) : state_(std::move(value)) {}
 
+    __AGENCY_ANNOTATION
     always_ready_future(std::exception_ptr e) : state_(e) {}
 
+    __AGENCY_ANNOTATION
     always_ready_future(always_ready_future&& other)
       : state_()
     {
       detail::adl_swap(state_, other.state_);
     }
 
+    // converting constructor waits on the given future and moves its result into *this
+    __agency_exec_check_disable__
+    template<class Future,
+             __AGENCY_REQUIRES(is_future<Future>::value),
+             __AGENCY_REQUIRES(std::is_constructible<T, future_value_t<Future>>::value)
+            >
+    __AGENCY_ANNOTATION
+    always_ready_future(Future& other)
+      : always_ready_future(other.get())
+    {}
+
+    __AGENCY_ANNOTATION
     always_ready_future& operator=(always_ready_future&& other)
     {
       state_.reset();
@@ -68,61 +90,104 @@ class always_ready_future
              __AGENCY_REQUIRES(
                std::is_constructible<T,U&&>::value
              )>
+    __AGENCY_ANNOTATION
     static always_ready_future make_ready(U&& value)
     {
       return always_ready_future(std::forward<U>(value));
     }
 
-  private:
-    struct get_visitor
+    __AGENCY_ANNOTATION
+    constexpr static bool is_ready()
     {
-      T operator()(T& value) const
+      return true;
+    }
+
+  private:
+    struct get_ptr_visitor
+    {
+      __AGENCY_ANNOTATION
+      T* operator()(T& result) const
       {
-        return std::move(value);
+        return &result;
       }
 
-      T operator()(std::exception_ptr& e) const
+      __AGENCY_ANNOTATION
+      T* operator()(std::exception_ptr& e) const
       {
+#ifndef __CUDA_ARCH__
         std::rethrow_exception(e);
-
-        // XXX rework this visitor to avoid returning T in both cases
-        return T();
+#else
+        detail::terminate_with_message("always_ready_future::get_ref(): error: exceptional future");
+#endif
+        return nullptr;
       }
     };
 
   public:
-    T get()
+    __AGENCY_ANNOTATION
+    T& get_ref()
     {
       if(!valid())
       {
+#ifndef __CUDA_ARCH__
         throw std::future_error(std::future_errc::no_state);
+#else
+        detail::terminate_with_message("always_ready_future::get_ref(): error: no state");
+#endif
       }
 
-      T result = experimental::visit(get_visitor(), *state_);
+      return *experimental::visit(get_ptr_visitor(), *state_);
+    }
 
-      state_ = experimental::nullopt;
+    __AGENCY_ANNOTATION
+    T get()
+    {
+      T result = std::move(get_ref());
+
+      invalidate();
 
       return result;
     }
 
+    __AGENCY_ANNOTATION
     void wait() const
     {
-      // wait() is a no-op: this is always ready
+      // wait() is a no-op: *this is always ready
     }
 
+    __AGENCY_ANNOTATION
     bool valid() const
     {
       return state_.has_value();
     }
 
     template<class Function>
+    __AGENCY_ANNOTATION
+    always_ready_future<detail::result_of_t<detail::decay_t<Function>(T&)>>
+      then_and_leave_valid(Function&& f)
+    {
+      return detail::always_ready_then_and_leave_valid(*this, std::forward<Function>(f));
+    }
+
+    template<class Function>
+    __AGENCY_ANNOTATION
     always_ready_future<detail::result_of_t<detail::decay_t<Function>(T&)>>
       then(Function&& f)
     {
-      return detail::always_ready_then(*this, std::forward<Function>(f));
+      auto result = then_and_leave_valid(std::forward<Function>(f));
+
+      invalidate();
+
+      return result;
     }
 
   private:
+    __AGENCY_ANNOTATION
+    void invalidate()
+    {
+      state_ = experimental::nullopt;
+    }
+
     experimental::optional<experimental::variant<T,std::exception_ptr>> state_;
 };
 
@@ -131,10 +196,12 @@ template<>
 class always_ready_future<void>
 {
   public:
+    __AGENCY_ANNOTATION
     always_ready_future() : valid_(true) {}
 
     always_ready_future(std::exception_ptr e) : exception_(e), valid_(true) {}
 
+    __AGENCY_ANNOTATION
     always_ready_future(always_ready_future&& other)
       : exception_(), valid_(false)
     {
@@ -142,6 +209,19 @@ class always_ready_future<void>
       detail::adl_swap(valid_, other.valid_);
     }
 
+    // converting constructor waits on the given future
+    template<class Future,
+             __AGENCY_REQUIRES(is_future<Future>::value),
+             __AGENCY_REQUIRES(std::is_void<future_value_t<Future>>::value)
+            >
+    __AGENCY_ANNOTATION
+    always_ready_future(Future& other)
+      : always_ready_future()
+    {
+      other.wait();
+    }
+
+    __AGENCY_ANNOTATION
     always_ready_future& operator=(always_ready_future&& other)
     {
       exception_.reset();
@@ -152,45 +232,80 @@ class always_ready_future<void>
       return *this;
     }
 
+    __AGENCY_ANNOTATION
+    constexpr static bool is_ready()
+    {
+      return true;
+    }
+
+    __AGENCY_ANNOTATION
     static always_ready_future make_ready()
     {
       return always_ready_future();
     }
 
   public:
+    __AGENCY_ANNOTATION
     void get()
     {
       if(!valid())
       {
+#ifndef __CUDA_ARCH__
         throw std::future_error(std::future_errc::no_state);
+#else
+        detail::terminate_with_message("always_ready_future::get(): error: no state");
+#endif
       }
 
-      valid_ = false;
+      invalidate();
 
       if(exception_)
       {
-        throw exception_.value();
+#ifndef __CUDA_ARCH__
+        std::rethrow_exception(exception_.value());
+#else
+        detail::terminate_with_message("always_ready_future::get(): error: exceptional future");
+#endif
       }
     }
 
+    __AGENCY_ANNOTATION
     void wait() const
     {
       // wait() is a no-op: this is always ready
     }
 
+    __AGENCY_ANNOTATION
     bool valid() const
     {
       return valid_;
     }
 
     template<class Function>
+    __AGENCY_ANNOTATION
+    always_ready_future<detail::result_of_t<detail::decay_t<Function>()>>
+      then_and_leave_valid(Function&& f)
+    {
+      return detail::always_ready_then_and_leave_valid(*this, std::forward<Function>(f));
+    }
+
+    template<class Function>
+    __AGENCY_ANNOTATION
     always_ready_future<detail::result_of_t<detail::decay_t<Function>()>>
       then(Function&& f)
     {
-      return detail::always_ready_then(*this, std::forward<Function>(f));
+      auto result = then_and_leave_valid(std::forward<Function>(f));
+      invalidate();
+      return result;
     }
 
   private:
+    __AGENCY_ANNOTATION
+    void invalidate()
+    {
+      valid_ = false;
+    }
+
     experimental::optional<std::exception_ptr> exception_;
 
     bool valid_;
@@ -198,11 +313,13 @@ class always_ready_future<void>
 
 
 template<class T>
+__AGENCY_ANNOTATION
 always_ready_future<detail::decay_t<T>> make_always_ready_future(T&& value)
 {
   return always_ready_future<detail::decay_t<T>>(std::forward<T>(value));
 }
 
+__AGENCY_ANNOTATION
 inline always_ready_future<void> make_always_ready_future()
 {
   return always_ready_future<void>();
@@ -216,16 +333,19 @@ always_ready_future<T> make_always_ready_exceptional_future(std::exception_ptr e
 }
 
 
+__agency_exec_check_disable__
 template<class Function,
          class... Args,
          __AGENCY_REQUIRES(
            !std::is_void<detail::result_of_t<Function&&(Args&&...)>>::value
          )>
+__AGENCY_ANNOTATION
 always_ready_future<
   detail::result_of_t<Function&&(Args&&...)>
 >
 try_make_always_ready_future(Function&& f, Args&&... args)
 {
+#ifndef __CUDA_ARCH__
   try
   {
     return make_always_ready_future(std::forward<Function>(f)(std::forward<Args>(args)...));
@@ -236,16 +356,22 @@ try_make_always_ready_future(Function&& f, Args&&... args)
 
     return make_always_ready_exceptional_future<result_type>(std::current_exception());
   }
+#else
+  return make_always_ready_future(std::forward<Function>(f)(std::forward<Args>(args)...));
+#endif
 }
 
 
+__agency_exec_check_disable__
 template<class Function,
          class... Args,
          __AGENCY_REQUIRES(
            std::is_void<detail::result_of_t<Function&&(Args&&...)>>::value
          )>
+__AGENCY_ANNOTATION
 always_ready_future<void> try_make_always_ready_future(Function&& f, Args&&... args)
 {
+#ifndef __CUDA_ARCH__
   try
   {
     std::forward<Function>(f)(std::forward<Args>(args)...);
@@ -255,6 +381,10 @@ always_ready_future<void> try_make_always_ready_future(Function&& f, Args&&... a
   {
     return make_always_ready_exceptional_future<void>(std::current_exception());
   }
+#else
+  std::forward<Function>(f)(std::forward<Args>(args)...);
+  return make_always_ready_future();
+#endif
 }
 
 
@@ -263,19 +393,20 @@ namespace detail
 
 
 template<class T, class Function>
+__AGENCY_ANNOTATION
 always_ready_future<detail::result_of_t<Function&&(T&)>>
-  always_ready_then(always_ready_future<T>& future, Function&& f)
+  always_ready_then_and_leave_valid(always_ready_future<T>& future, Function&& f)
 {
-  auto argument = future.get();
+  T& argument = future.get_ref();
   return agency::try_make_always_ready_future(std::forward<Function>(f), argument);
 }
 
 
 template<class Function>
+__AGENCY_ANNOTATION
 always_ready_future<detail::result_of_t<Function&&()>>
-  always_ready_then(always_ready_future<void>& future, Function&& f)
+  always_ready_then_and_leave_valid(always_ready_future<void>& future, Function&& f)
 {
-  future.get();
   return agency::try_make_always_ready_future(std::forward<Function>(f));
 }
 
