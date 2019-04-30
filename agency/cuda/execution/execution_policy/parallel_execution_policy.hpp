3d5fe421119571eee2486cab1fe103fde879f031
Jared Hoberock <jaredhoberock@gmail.com>
2019-04-30 12:34:49 -0500

    Collapse implementations of cuda::replace_policy Generalize cuda::replace_policy to multiple dimensions

--- agency/cuda/execution/execution_policy/parallel_execution_policy.hpp
+++ agency/cuda/execution/execution_policy/parallel_execution_policy.hpp
@@ -42,92 +42,43 @@ const parallel_execution_policy_2d par2d{};
 
 template<class ParallelPolicy,
          __AGENCY_REQUIRES(
-           agency::detail::policy_is_parallel<ParallelPolicy>::value and
-           agency::detail::execution_policy_dimensionality<ParallelPolicy>::value == 1
-         )>
+           agency::detail::policy_is_parallel<ParallelPolicy>::value
+        )>
 __AGENCY_ANNOTATION
-cuda::parallel_execution_policy replace_executor(const ParallelPolicy& policy, const parallel_executor& exec)
+auto replace_executor(const ParallelPolicy& policy, const grid_executor& exec)
+  -> decltype(agency::replace_executor(policy, cuda::parallel_executor(exec)))
 {
-  return cuda::parallel_execution_policy(policy.param(), exec);
-}
-
-
-template<class ParallelPolicy,
-         __AGENCY_REQUIRES(
-           agency::detail::policy_is_parallel<ParallelPolicy>::value and
-           agency::detail::execution_policy_dimensionality<ParallelPolicy>::value == 2
-         )>
-__AGENCY_ANNOTATION
-cuda::parallel_execution_policy_2d replace_executor(const ParallelPolicy& policy, const parallel_executor& exec)
-{
-  return cuda::parallel_execution_policy_2d(policy.param(), exec);
-}
-
-
-template<class ParallelPolicy,
-         __AGENCY_REQUIRES(
-           agency::detail::policy_is_parallel<ParallelPolicy>::value and
-           agency::detail::execution_policy_dimensionality<ParallelPolicy>::value == 1
-         )>
-__AGENCY_ANNOTATION
-cuda::parallel_execution_policy replace_executor(const ParallelPolicy& policy, const grid_executor& exec)
-{
-  // "flatten" the grid_executor into a parallel_executor
-  cuda::parallel_executor parallel_exec(exec);
-
-  // call a lower-level version of replace_executor()
-  return cuda::replace_executor(policy, parallel_exec);
-}
-
-
-template<class ParallelPolicy,
-         __AGENCY_REQUIRES(
-           agency::detail::policy_is_parallel<ParallelPolicy>::value and
-           agency::detail::execution_policy_dimensionality<ParallelPolicy>::value == 2
-         )>
-__AGENCY_ANNOTATION
-cuda::parallel_execution_policy_2d replace_executor(const ParallelPolicy& policy, const grid_executor& exec)
-{
-  // "flatten" the grid_executor into a parallel_executor
-  cuda::parallel_executor parallel_exec(exec);
-
-  // call a lower-level version of replace_executor()
-  return cuda::replace_executor(policy, parallel_exec);
+  // create a parallel_executor from the grid_executor
+  return agency::replace_executor(policy, cuda::parallel_executor(exec));
 }
 
 
 // this overload is called on e.g. par.on(device(0))
-// XXX this function needs to account for the dimensionality of ParallelPolicy's agents
 template<class ParallelPolicy,
          __AGENCY_REQUIRES(
            agency::detail::policy_is_parallel<ParallelPolicy>::value
          )>
 __AGENCY_ANNOTATION
-cuda::parallel_execution_policy replace_executor(const ParallelPolicy& policy, device_id device)
+auto replace_executor(const ParallelPolicy& policy, device_id device)
+  -> decltype(agency::replace_executor(policy, cuda::grid_executor(device)))
 {
   // create a grid_executor from the device_id
-  cuda::grid_executor grid_exec(device);
-
-  // call a lower-level version of replace_executor()
-  return cuda::replace_executor(policy, grid_exec);
+  return agency::replace_executor(policy, cuda::grid_executor(device));
 }
 
 
 // this overload is called on e.g. par.on(all_devices())
-// XXX this function needs to account for the dimensionality of ParallelPolicy's agents
 template<class ParallelPolicy,
          class Range,
          __AGENCY_REQUIRES(
            detail::is_range_of_device_id<Range>::value and
            agency::detail::policy_is_parallel<ParallelPolicy>::value
          )>
-basic_execution_policy<parallel_agent, multidevice_executor> replace_executor(const ParallelPolicy& policy, const Range& devices)
+auto replace_executor(const ParallelPolicy& policy, const Range& devices)
+  -> decltype(agency::replace_executor(policy, multidevice_executor(detail::devices_to_grid_executors(devices))))
 {
   // turn the range of device_id into a vector of grid_executors
-  auto grid_executors = agency::cuda::detail::devices_to_grid_executors(devices);
-  multidevice_executor exec(grid_executors);
-
-  return policy.on(exec);
+  return agency::replace_executor(policy, multidevice_executor(detail::devices_to_grid_executors(devices)));
 }
 
 
